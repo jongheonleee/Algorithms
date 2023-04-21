990d67a58f46455c45bc03f49372caf732a2e7e6
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-04-21 22:33:39 +0900

    [Silver III] Title: N과 M (8), Time: 216 ms, Memory: 18892 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static Integer[] a;
+    static StringBuilder sb = new StringBuilder();
+    static String[] line;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]);
+        a = new Integer[n];
+        line = br.readLine().split(" ");
+        for (int i=0; i<n; i++) {
+            a[i] = Integer.parseInt(line[i]);
+        }
+
+        // 정렬시키기
+        Arrays.sort(a);
+        go(0, m, "");
+        System.out.println(sb);
+
+    }
+
+    public static void go(int start, int m, String sequence) {
+        if (m == 0) {
+            sb.append(sequence).append("\n");
+            return;
+        }
+
+        for (int i=start; i<a.length; i++) {
+            go(i, m-1, sequence + (a[i] + " "));
+        }
+    }
+
+}
