958fa5d3374c328847369762fba9a28e6a767a2b
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-04-22 11:55:55 +0900

    [Silver II] Title: N과 M (11), Time: 1256 ms, Memory: 151812 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,87 @@
+import java.util.*;
+class Result implements Comparable<Result> {
+    Integer[] a;
+    Result(ArrayList<Integer> a) {
+        this.a = a.toArray(new Integer[a.size()]);
+    }
+
+    int get(int i) {
+        return (int)this.a[i];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Result) {
+            Result that = (Result)o;
+            int n = this.a.length;
+
+            for (int i=0; i<n; i++) {
+                if (this.a[i] != that.a[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int compareTo(Result that) {
+        int n = this.a.length;
+        for(int i=0; i<n; i++) {
+            if (this.a[i] == that.a[i]) continue;
+            if (this.a[i] < that.a[i]) return -1;
+            if (this.a[i] > that.a[i]) return 1;
+        }
+        return 0;
+    }
+}
+public class Main {
+
+    static int[] a = new int[10];
+    static int[] num = new int[10];
+    static ArrayList<Result> d = new ArrayList<>();
+
+    static void go(int i, int n, int m) {
+        if (i == m) {
+            ArrayList<Integer> tmp = new ArrayList<>();
+            for (int j=0; j<m; j++) {
+                tmp.add(num[a[j]]);
+            }
+            d.add(new Result(tmp));
+            return;
+        }
+
+        for (int j=0; j<n; j++) {
+            if (a[i] != 0) continue;
+            a[i] = j;
+            go(i+1,  n, m);
+            a[i] = 0;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i=0; i<n; i++)
+            num[i] = sc.nextInt();
+
+        Arrays.sort(num, 0, n);
+        go(0, n, m);
+        Collections.sort(d);
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<d.size(); i++){
+            if (i == 0 || !d.get(i).equals(d.get(i-1))) {
+                for (int j=0; j<m; j++) {
+                    sb.append(d.get(i).get(j));
+                    if (j != m-1) sb.append(' ');
+                }
+                sb.append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
