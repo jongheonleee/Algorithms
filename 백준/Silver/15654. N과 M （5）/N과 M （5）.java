65b2eb10752e0166b8b7575f1beb9f4e41b4efa1
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-04-25 17:18:13 +0900

    [Silver III] Title: N과 M (5), Time: 360 ms, Memory: 29472 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,48 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.util.*;
 public class Main {
-
-    static int[] a;
-    static String[] line;
+    static int[] a = new int[10];
+    static int [] num = new int[10];
+    static boolean[] c = new boolean[10];
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        line = br.readLine().split(" ");
-        int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]);
-        line = br.readLine().split(" ");
-        a = new int[n];
 
-        for (int i=0; i<n; i++) {
-            a[i] = Integer.parseInt(line[i]);
-        }
-        Arrays.sort(a);
-        dfs(m, "");
-        System.out.println(sb);
-
-    }
-
-    public static void dfs(int m, String crnt) {
-        if (m == 0) {
-            sb.append(crnt).append("\n");
+    public static void go(int idx, int n, int m) {
+        if (idx == m) {
+            for (int i=0; i<m; i++) {
+                sb.append(num[a[i]]);
+                if (i != m-1) sb.append(" ");
+            }
+            sb.append("\n");
             return;
         }
 
-        for (int i=0; i<=a.length-1; i++) {
-            if (a[i] != -1) {
-                int tmp = a[i];
-                String prev = crnt;
-
-                crnt += tmp + " ";
-                a[i] = -1;
-                dfs(m-1, crnt);
-
-                crnt = prev;
-                a[i] = tmp;
-            }
+        for (int i=0; i<n; i++) {
+            if (c[i]) continue;
+            c[i] = true; a[idx] = i;
+            go(idx+1, n, m);
+            c[i] = false;
         }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line1 = br.readLine().split(" ");
+        int n = Integer.parseInt(line1[0]), m = Integer.parseInt(line1[1]);
+        String[] line2 = br.readLine().split(" ");
+        for (int i=0; i<n; i++) {
+            num[i] = Integer.parseInt(line2[i]);
+        }
+        Arrays.sort(num, 0, n);
+        go(0, n, m);
+        System.out.println(sb);
+
     }
 }
