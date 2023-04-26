ffc2202e2335eb5e1381633bb45ece679aa1ccbf
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-04-26 23:06:13 +0900

    [Silver II] Title: 외판원 순회 2, Time: 332 ms, Memory: 19096 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,74 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
 public class Main {
 
-    public static boolean next_permutation(int[] a) {
-        // 1. 최대 길이의 마지막 부분 수열을 찾음
-        int i = a.length-1;
-        while (i>0 && a[i-1] >= a[i]) {
-            i--;
-        }
+    static int[] a = new int[10];
+    public static boolean next_permutation(int[] a, int n) {
+        int i = n-1;
+        while (i>0 && a[i-1] >= a[i]) i--;
 
-        if (i<=0) {
-            return false;
-        }
+        if (i <= 0) return false;
 
-        // 2. a[i-1]와 a[j] 스왑
-        // 이때, a[j] >= a[i-1]을 만족하는 가장 큰 j
-        int j = a.length-1;
-        while (a[j] <= a[i-1]) {
-            j--;
-        }
+        int j = n-1;
+        while (a[i-1] >= a[j]) j--;
+
         int tmp = a[i-1];
-        a[i-1] = a[j];
-        a[j] = tmp;
+        a[i-1] = a[j]; a[j] = tmp;
 
-        // 3. 마지막 수열 부분을 첫 수열 부분으로, 즉 내림차순을 뒤집어서 오름차순으로 만들기
-        j = a.length-1;
-        while (i < j) {
+        j = n-1;
+        while(i < j) {
             tmp = a[i];
             a[i] = a[j]; a[j] = tmp;
             i++; j--;
         }
+
         return true;
-
     }
-
 
     public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] w = new int[n][n];
         for (int i=0; i<n; i++) {
+            a[i] = i;
+        }
+
+        for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 w[i][j] = sc.nextInt();
             }
         }
-        int ans = Integer.MAX_VALUE;
-        int[] a = new int[n];
-        for (int i=0; i<n; i++) {
-            a[i] = i;
-        }
+        int min = Integer.MAX_VALUE;
 
         do {
             boolean ok = true;
-            int sum = 0;
+            int tmp = 0;
             for (int i=0; i<n-1; i++) {
                 if (w[a[i]][a[i+1]] == 0) {
                     ok = false;
                 } else {
-                    sum += w[a[i]][a[i+1]];
+                    tmp += w[a[i]][a[i+1]];
                 }
             }
             if (ok && w[a[n-1]][a[0]] != 0) {
-                sum += w[a[n-1]][a[0]];
-                if (ans > sum) ans = sum;
+                tmp += w[a[n-1]][a[0]];
+            } else {
+                ok = false;
             }
-        } while (next_permutation(a));
-        System.out.println(ans);
+
+            if (ok) {
+                min = Math.min(min, tmp);
+            }
+
+        } while (next_permutation(a, n));
+        System.out.println(min);
 
     }
 }
