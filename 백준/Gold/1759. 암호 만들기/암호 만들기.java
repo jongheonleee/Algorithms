0ed7a6cc139728a033aabb3edee1c4f22df471f0
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-04-28 20:58:18 +0900

    [Gold V] Title: 암호 만들기, Time: 152 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,41 +1,58 @@
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+
 
 public class Main {
-    public static boolean check(String password) {
-        int ja = 0;
-        int mo = 0;
-        for (char x : password.toCharArray()) {
-            if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
-                mo += 1;
-            } else {
-                ja += 1;
-            }
-        }
-        return ja >= 2 && mo >= 1;
-    }
-    public static void go(int n, String[] alpha, String password, int i) {
-        if (password.length() == n) {
-            if (check(password)) {
-                System.out.println(password);
+
+    static StringBuilder sb = new StringBuilder();
+    static String[] a;
+
+    static void go(int i, String pw, int l) {
+        // 정답을 찾은 경우
+        if (pw.length() == l) {
+            if (check(pw)) {
+                sb.append(pw).append("\n");
             }
             return;
         }
-        if (i >= alpha.length) return;
-        go(n, alpha, password+alpha[i], i+1);
-        go(n, alpha, password, i+1);
+
+        // 종료 조건
+        if (i > a.length-1) return;
+
+        // 재귀 호출 진행
+        // a[i]를 선택한 경우
+        go(i+1, pw+a[i], l);
+        // a[i]를 선택하지 않은 경우
+        go(i+1, pw, l);
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        
-        String[] alpha = new String[m];
-        for (int i=0; i<m; i++) {
-            alpha[i] = sc.next();
+
+    // 최소 한 개의 모음 두 개의 자음으로 구성되어 있는지 확인
+    static boolean check(String pw) {
+        int mo = 0, ja = 0;
+
+        for (char c : pw.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                mo++;
+            }
+            else {
+                ja++;
+            }
         }
-        
-        Arrays.sort(alpha);
-        
-        go(n, alpha, "", 0);
+        return mo >= 1 && ja >= 2;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line1 = br.readLine().split(" ");
+        String[] line2 = br.readLine().split(" ");
+        int l = Integer.parseInt(line1[0]), c = Integer.parseInt(line1[1]);
+        a = new String[c];
+        for (int i=0; i<c; i++) a[i] = line2[i];
+        Arrays.sort(a);
+
+        go(0, "", l);
+        System.out.println(sb);
+
+
     }
 }
