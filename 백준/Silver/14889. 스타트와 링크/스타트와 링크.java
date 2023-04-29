6a1fe8f4dbe4c126cc937867a80f3fe1b096ee79
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-04-29 11:55:44 +0900

    [Silver II] Title: 스타트와 링크, Time: 292 ms, Memory: 20044 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,51 +1,65 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int[][] s;
     static int n;
-    static int go(int index, ArrayList<Integer> first, ArrayList<Integer> second) {
-        if (index == n) {
-            if (first.size() != n/2) return -1;
-            if (second.size() != n/2) return -1;
-            int t1 = 0;
-            int t2 = 0;
+    static int[][] s;
+    static int go(int num, ArrayList<Integer> first, ArrayList<Integer> second) {
+        // 정답을 찾은 경우 or 종료 경우
+        if (num == n) {
+            // 종료 경우
+            if (first.size() != n/2 || second.size() != n/2) return -1;
+
+            // 정답을 찾은 경우
+            int sum1 = 0, sum2 = 0;
             for (int i=0; i<n/2; i++) {
                 for (int j=0; j<n/2; j++) {
-                    if (i == j) continue;
-                    t1 += s[first.get(i)][first.get(j)];
-                    t2 += s[second.get(i)][second.get(j)];
+                    sum1 += s[first.get(i)][first.get(j)];
+                    sum2 += s[second.get(i)][second.get(j)];
                 }
             }
-            int diff = Math.abs(t1-t2);
+
+            int diff = Math.abs(sum1 - sum2);
             return diff;
         }
-        if (first.size() > n/2) return -1;
-        if (second.size() > n/2) return -1;
-        int ans = -1;
-        first.add(index);
-        int t1 = go(index+1, first, second);
-        if (ans == -1 || (t1 != -1 && ans > t1)) {
-            ans = t1;
+
+        // 재귀 호출하는 경우
+        // 첫 번째 팀을 선택하는 경우
+        int result = -1;
+        first.add(num);
+        int v1 = go(num+1, first, second);
+        if (result == -1 || v1 != -1 && result > v1) {
+            result = v1;
         }
         first.remove(first.size()-1);
-        second.add(index);
-        int t2 = go(index+1, first, second);
-        if (ans == -1 || (t2 != -1 && ans > t2)) {
-            ans = t2;
+
+        // 두 번째 팀을 선택하는 경우
+        second.add(num);
+        int v2 = go(num+1, first, second);
+        if (result == -1 || v2 != -1 && result > v2) {
+            result = v2;
         }
         second.remove(second.size()-1);
-        return ans;
+
+        return result;
+
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
         s = new int[n][n];
+        String[] line;
+
         for (int i=0; i<n; i++) {
+            line = br.readLine().split(" ");
             for (int j=0; j<n; j++) {
-                s[i][j] = sc.nextInt();
+                s[i][j] = Integer.parseInt(line[j]);
             }
         }
-        ArrayList<Integer> first = new ArrayList<>();
-        ArrayList<Integer> second = new ArrayList<>();
-        System.out.println(go(0, first, second));
+
+        ArrayList<Integer> first = new ArrayList<>(), second = new ArrayList<>();
+        int result = go(0, first, second);
+        System.out.println(result);
     }
 }
