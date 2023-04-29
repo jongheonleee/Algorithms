0f1a27b813ac9875e9029eb4b0daea0968f7353c
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-04-30 00:28:14 +0900

    [Silver I] Title: 부등호, Time: 228 ms, Memory: 26936 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -3,44 +3,45 @@ import java.io.*;
 public class Main {
 
     static int n;
+    static char[] a;
     static boolean[] check = new boolean[10];
     static ArrayList<String> result = new ArrayList<>();
-    static char[] a;
+
+    static boolean good(char x, char y, char op) {
+        if (op == '>') {
+            if (x < y) return false;
+        }
+        else {
+            if (x > y) return false;
+        }
+
+        return true;
+    }
+
     static void go(int idx, String num) {
-        // 종료 & 정답을 찾은 경우
+        // 정답을 찾은 경우
         if (idx == n+1) {
-            if (ok(num)) {
-                result.add(num);
-            }
+            result.add(num);
             return;
         }
 
-        // 재귀를 호출하는 경우
         for (int i=0; i<=9; i++) {
             if (check[i]) continue;
-            check[i] = true;
-            go(idx+1, num + Integer.toString(i));
-            check[i] = false;
+
+            if (idx == 0 || good(num.charAt(idx-1), (char)(i+'0'), a[idx-1])) {
+                check[i] = true;
+                go(idx+1, num + Integer.toString(i));
+                check[i] = false;
+            }
         }
     }
 
-    static boolean ok(String num) {
-        for (int i=0; i<n; i++) {
-            if (a[i] == '>') {
-                if (num.charAt(i) < num.charAt(i+1)) return false;
-            } else {
-                if (num.charAt(i) > num.charAt(i+1)) return false;
-            }
-        }
-        return true;
-    }
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         a = new char[n];
         String[] line = br.readLine().split(" ");
-
         for (int i=0; i<n; i++) {
             a[i] = line[i].charAt(0);
         }
