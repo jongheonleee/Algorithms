27c8d9b0764b2e1237fb5b19fe265c5981a13ebb
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-02 17:08:57 +0900

    [Gold III] Title: Guess, Time: 216 ms, Memory: 15940 KB -BaekjoonHub

--- 백준/Gold/1248. Guess/Guess.java
+++ 백준/Gold/1248. Guess/Guess.java
@@ -1,60 +1,66 @@
 import java.util.*;
+import java.io.*;
 public class Main {
     static int n;
     static int[][] sign;
     static int[] ans;
-    static boolean check(int index) {
+
+    static boolean check(int idx) {
         int sum = 0;
-        for (int i=index; i>=0; i--) {
+        for (int i=idx; i>=0; i--) {
             sum += ans[i];
-            if (sign[i][index] == 0) {
+            if (sign[i][idx] == 0) {
                 if (sum != 0) return false;
-            } else if (sign[i][index] < 0) {
+            } else if (sign[i][idx] < 0) {
                 if (sum >= 0) return false;
-            } else if (sign[i][index] > 0) {
+            } else {
                 if (sum <= 0) return false;
             }
         }
+
         return true;
     }
-    static boolean go(int index) {
-        if (index == n) {
-            return true;
+
+    static boolean go(int idx) {
+        if (idx == n) return true;
+
+        if (sign[idx][idx] == 0) {
+            ans[idx] = 0;
+            return check(idx) & go(idx+1);
         }
-        if (sign[index][index] == 0) {
-            ans[index] = 0;
-            return check(index) && go(index+1);
-        }
+
         for (int i=1; i<=10; i++) {
-            ans[index] = sign[index][index]*i;
-            if (check(index) && go(index+1)) return true;
+            ans[idx] = sign[idx][idx]*i;
+            if (check(idx) && go(idx+1)) return true;
         }
+
         return false;
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
         ans = new int[n];
         sign = new int[n][n];
-        String s = sc.next();
+
+        String input = br.readLine();
         int cnt = 0;
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<n; i++){
             for (int j=i; j<n; j++) {
-                char x = s.charAt(cnt);
-                if (x == '0') {
-                    sign[i][j] = 0;
-                } else if (x == '+') {
-                    sign[i][j] = 1;
-                } else {
-                    sign[i][j] = -1;
-                }
-                cnt += 1;
+                char x = input.charAt(cnt);
+
+                if (x == '0') sign[i][j] = 0;
+                if (x == '+') sign[i][j] = 1;
+                if (x == '-') sign[i][j] = -1;
+
+                cnt++;
             }
         }
         go(0);
         for (int i=0; i<n; i++) {
-            System.out.print(ans[i] + " ");
+            sb.append(ans[i]).append(" ");
         }
-        System.out.println();
+        System.out.println(sb);
     }
 }
