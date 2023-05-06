e632525d9b5a588f650e6dae5d8be2e6965a47c1
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-06 12:42:59 +0900

    [Gold V] Title: 합분해, Time: 144 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,21 +1,18 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     static long mod = 1000000000;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line = br.readLine().split(" ");
         int n = Integer.parseInt(line[0]), k = Integer.parseInt(line[1]);
         long[][] d = new long[k+1][n+1];
 
-        // 초기 세팅값
         for (int i=0; i<=n; i++) {
             d[1][i] = 1;
         }
 
-        // 알고리즘 적용
+
         for (int i=2; i<=k; i++) {
             for (int j=0; j<=n; j++) {
                 for (int l=0; l<=j; l++) {
@@ -25,7 +22,5 @@ public class Main {
             }
         }
         System.out.println(d[k][n]);
-
-
     }
 }
