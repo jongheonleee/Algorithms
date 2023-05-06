b2335ac21f0b52efcaa2303a21694a2ade4cd09b
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-06 15:25:54 +0900

    [Silver II] Title: DFS와 BFS, Time: 264 ms, Memory: 21240 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,58 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] a;
-    static boolean[] c;
-    public static void dfs(int x) {
-        if (c[x]) {
-            return;
-        }
-        c[x] = true;
-        System.out.print(x + " ");
-        for (int y : a[x]) {
-            if (c[y] == false) {
-                dfs(y);
+
+    static StringBuilder sbDfs = new StringBuilder(), sbBfs = new StringBuilder();
+    static boolean[] check;
+    static ArrayList<Integer>[] graph;
+
+    public static void dfs(int v) {
+        if (check[v]) return;
+
+        check[v] = true;
+        sbDfs.append(v).append(" ");
+
+        for (int next : graph[v]) {
+            if (!check[next]) {
+                dfs(next);
             }
         }
     }
-    public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(start);
-        c[start] = true;
-        while (!q.isEmpty()) {
-            int x = q.remove();
-            System.out.print(x + " ");
-            for (int y : a[x]) {
-                if (c[y] == false) {
-                    c[y] = true;
-                    q.add(y);
+
+    public static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        check[v] = true;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.remove();
+            sbBfs.append(curr).append(" ");
+
+            for (int next : graph[curr]) {
+                if (!check[next]) {
+                    check[next] = true;
+                    queue.add(next);
                 }
             }
         }
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int start = sc.nextInt();
-        a = (ArrayList<Integer>[]) new ArrayList[n+1];
+
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]), src = Integer.parseInt(line[2]);
+
+        graph = (ArrayList<Integer>[]) new ArrayList[n+1];
         for (int i=1; i<=n; i++) {
-            a[i] = new ArrayList<Integer>();
+            graph[i] = new ArrayList<Integer>();
         }
+
         for (int i=0; i<m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            a[u].add(v);
-            a[v].add(u);
+            line = br.readLine().split(" ");
+            int u = Integer.parseInt(line[0]), v = Integer.parseInt(line[1]);
+            // 간선은 양방향
+            graph[u].add(v);
+            graph[v].add(u);
         }
+
         for (int i=1; i<=n; i++) {
-            Collections.sort(a[i]);
+            Collections.sort(graph[i]);
         }
-        c = new boolean[n+1];
-        dfs(start);
-        System.out.println();
-        c = new boolean[n+1];
-        bfs(start);
-        System.out.println();
+
+        check = new boolean[n+1];
+        dfs(src);
+        check = new boolean[n+1];
+        bfs(src);
+        System.out.println(sbDfs);
+        System.out.println(sbBfs);
+
     }
 }
