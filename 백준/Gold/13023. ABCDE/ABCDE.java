4de0079b9f83c7870e4e12377ac0250b851158e1
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-08 17:34:28 +0900

    [Gold V] Title: ABCDE, Time: 432 ms, Memory: 22496 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,58 +1,59 @@
 import java.util.*;
+import java.io.*;
+
+// 간선 리스트를 구현할 때 사용하는 노드
 class Edge {
     int from, to;
+
     Edge(int from, int to) {
         this.from = from;
         this.to = to;
     }
 }
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        // 인접 행렬
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]);
+        // 인접 행렬부터 구현
         boolean[][] a = new boolean[n][n];
-        // 간선 리스트
-        ArrayList<Integer>[] g = (ArrayList<Integer>[]) new ArrayList[n];
-        // 인접 리스트
-        ArrayList<Edge> edges = new ArrayList<Edge>();
+
+        // 인접 리스트 구현
+        ArrayList<Integer>[] graph = (ArrayList<Integer>[]) new ArrayList[n];
 
         for (int i=0; i<n; i++) {
-            g[i] = new ArrayList<Integer>();
+            graph[i] = new ArrayList<Integer>();
         }
+
+        // 간선 리스트 구현
+        ArrayList<Edge> edges = new ArrayList<>();
 
         for (int i=0; i<m; i++) {
-            int from = sc.nextInt();
-            int to = sc.nextInt();
+            line = br.readLine().split(" ");
+            int from = Integer.parseInt(line[0]), to = Integer.parseInt(line[1]);
+            a[from][to] = a[to][from] = true;
+            graph[from].add(to); graph[to].add(from);
             edges.add(new Edge(from, to));
             edges.add(new Edge(to, from));
-            a[from][to] = a[to][from] = true;
-            g[from].add(to);
-            g[to].add(from);
         }
 
-        // 이부분 코드의 의도를 잘 모르겠음
         m *= 2;
-        for  (int i=0; i<m; i++) {
+        for (int i=0; i<m; i++) {
             for (int j=0; j<m; j++) {
                 int A = edges.get(i).from;
                 int B = edges.get(i).to;
                 int C = edges.get(j).from;
                 int D = edges.get(j).to;
-                // 연결된 형태가 아니라 같은 경우, 즉 연결이 안된 경우(간선 리스트)
-                if (A == B || A == C || A == D || B == C || B == D || C == D) {
-                    continue;
-                }
-                // B-C 연결이 안된 경우(인접 행렬)
-                if (!a[B][C]) continue;
 
-                // (인접 리스트)
-                for (int E : g[D]) {
-                    // 연결된 형태가 아니라 같은 경우, 즉 연결이 안된 경우
-                    if (A == E || B == E || C == E || D == E) {
-                        continue;
-                    }
+                if (A == B || A == C || A == D ||B == C || B == D || C == D)
+                    continue;
+
+                if (!a[B][C])
+                    continue;
+
+                for (int E : graph[D]) {
+                    if (E == A || E == B || E == C || E == D) continue;
                     System.out.println(1);
                     System.exit(0);
                 }
