e0ff058c51aa216e9b3f9943abfe9d41f84a5601
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-09 14:50:32 +0900

    [Silver I] Title: 미로 탐색, Time: 144 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,51 +1,60 @@
 import java.util.*;
+import java.io.*;
 
 class Pair {
-    int x;
-    int y;
+    int x, y;
+
     Pair(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
 public class Main {
+
     public static final int[] dx = {0, 0, 1, -1};
     public static final int[] dy = {1, -1, 0, 0};
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line1 = br.readLine().split(" ");
+        int n = Integer.parseInt(line1[0]), m = Integer.parseInt(line1[1]);
+
         int[][] a = new int[n][m];
-        sc.nextLine();
-        for (int i=0; i<n; i++) {
-            String s = sc.nextLine();
-            for (int j=0; j<m; j++) {
-                a[i][j] = s.charAt(j) - '0';
-            }
-        }
         int[][] dist = new int[n][m];
         boolean[][] check = new boolean[n][m];
+
+        for (int i=0; i<n; i++) {
+            String line2 = br.readLine();
+            for (int j=0; j<m; j++) {
+                a[i][j] = line2.charAt(j) - '0';
+            }
+        }
+
         Queue<Pair> q = new LinkedList<Pair>();
         q.add(new Pair(0, 0));
-        check[0][0] = true;
         dist[0][0] = 1;
+        check[0][0] = true;
+
         while (!q.isEmpty()) {
             Pair p = q.remove();
-            int x = p.x;
-            int y = p.y;
-            for (int k=0; k<4; k++) {
-                int nx = x+dx[k];
-                int ny = y+dy[k];
+            int x = p.x, y = p.y;
+
+            if (x == n-1 && y == m-1)
+                break;
+
+            for (int k=0; k<4; k++){
+                int nx = x+dx[k], ny = y+dy[k];
+
                 if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (check[nx][ny] == false && a[nx][ny] == 1) {
+                    if (a[nx][ny] == 1 && check[nx][ny] == false) {
                         q.add(new Pair(nx, ny));
-                        dist[nx][ny] = dist[x][y] + 1;
                         check[nx][ny] = true;
+                        dist[nx][ny] = dist[x][y] + 1;
                     }
                 }
             }
         }
         System.out.println(dist[n-1][m-1]);
+
     }
 }
