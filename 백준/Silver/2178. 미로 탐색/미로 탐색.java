a3ecda769d80e5cfa600198d0aed9bf8d43cc18c
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-10 11:23:37 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,57 +4,59 @@ import java.io.*;
 class Pair {
     int x, y;
 
-    Pair(int x, int y) {
+    Pair (int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
+
 public class Main {
 
-    public static final int[] dx = {0, 0, 1, -1};
-    public static final int[] dy = {1, -1, 0, 0};
+    private static final int[] dx = {0, 0, 1, -1};
+    private static final int[] dy = {1, -1, 0, 0};
+
+    public static void bfs(int[][] a, int[][] cnt, int x, int y, int n, int m) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(x, y));
+        cnt[x][y] = 1;
+
+        while (!q.isEmpty()) {
+            Pair p = q.remove();
+            x = p.x; y = p.y;
+
+            for (int k=0; k<4; k++) {
+                int nx = x+dx[k], ny = y+dy[k];
+
+                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                    if (a[nx][ny] == 1 && cnt[nx][ny] == -1){
+                        q.add(new Pair(nx, ny));
+                        cnt[nx][ny] = cnt[x][y]+1;
+                    }
+
+                }
+            }
+        }
+    }
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line1 = br.readLine().split(" ");
-        int n = Integer.parseInt(line1[0]), m = Integer.parseInt(line1[1]);
+        String[] line = br.readLine().split(" ");
+
+        int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]);
 
         int[][] a = new int[n][m];
-        int[][] dist = new int[n][m];
-        boolean[][] check = new boolean[n][m];
+        int[][] cnt = new int[n][m];
 
         for (int i=0; i<n; i++) {
             String line2 = br.readLine();
             for (int j=0; j<m; j++) {
                 a[i][j] = line2.charAt(j) - '0';
+                cnt[i][j] = -1;
             }
         }
 
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(0, 0));
-        dist[0][0] = 1;
-        check[0][0] = true;
-
-        while (!q.isEmpty()) {
-            Pair p = q.remove();
-            int x = p.x, y = p.y;
-
-            if (x == n-1 && y == m-1)
-                break;
-
-            for (int k=0; k<4; k++){
-                int nx = x+dx[k], ny = y+dy[k];
-
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (a[nx][ny] == 1 && check[nx][ny] == false) {
-                        q.add(new Pair(nx, ny));
-                        check[nx][ny] = true;
-                        dist[nx][ny] = dist[x][y] + 1;
-                    }
-                }
-            }
-        }
-        System.out.println(dist[n-1][m-1]);
+        bfs(a, cnt, 0, 0, n, m);
+        System.out.println(cnt[n-1][m-1]);
 
     }
 }
