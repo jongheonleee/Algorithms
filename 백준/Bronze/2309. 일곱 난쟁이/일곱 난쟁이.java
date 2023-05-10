5521b7627471f904923a20c2e09921278c95f9d1
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-10 12:20:32 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,38 +1,38 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int[] h = new int[9];
-        for(int i=0; i<9; i++) h[i] = Integer.parseInt(br.readLine());
+        int[] cand = new int[9], ans = new int[7];
+        int sum = 0;
 
-        // 전체 합
-        int total = 0, diff = 0;
-        for (int i=0; i<9; i++) total += h[i];
+        for (int i=0; i<9; i++) {
+            int h = Integer.parseInt(br.readLine());
+            cand[i] = h;
+            sum += cand[i];
+        }
 
-        // 차이 = 전체 - 100
-        diff = total - 100;
+        for (int i=0; i<8; i++) {
+            for (int j=i+1; j<9; j++) {
+                int tmp = cand[i] + cand[j];
 
-        // 밑에 알고리즘의 예외 발생을 막기 위해 정렬해주기
-        Arrays.sort(h);
-        // 2명 키의 합이 diff인 경우 찾기
-        for (int i=0; i<h.length; i++) {
-            for (int j=i+1; j<h.length; j++) {
-                if (diff == h[i] + h[j]) {
-                    for (int k=0; k<h.length; k++) {
-                        if ( k != i && k != j) {
-                            sb.append(h[k]).append("\n");
+                if (100 == sum - tmp) {
+                    int l = 0;
+                    for (int k=0; k<9; k++) {
+                        if (k != i && k != j) {
+                            ans[l] = cand[k];
+                            l++;
                         }
-                    }        
-										// 출력하기
-						        System.out.println(sb);
-										System.exit(0);
-									
+                    }
+                    break;
                 }
             }
+        }
+        Arrays.sort(ans);
+        for (int i=0; i<7; i++) {
+            System.out.println(ans[i]);
         }
 
     }
