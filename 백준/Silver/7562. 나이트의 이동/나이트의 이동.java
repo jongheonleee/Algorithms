3bdc27491f5b88a6c4613cf47028219bbe0c9f56
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-10 12:01:00 +0900

    [Silver I] Title: 나이트의 이동, Time: 360 ms, Memory: 74072 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -4,60 +4,61 @@ import java.io.*;
 class Pair {
     int x, y;
 
-    Pair(int x, int y) {
+    Pair (int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
+
 public class Main {
 
-    public static final int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
-    public static final int[] dy = {2, 1, -1, -2, -2, -1, 1, 2};
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+    private static final int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
+    private static final int[] dy = {2, 1, -1, -2, -2, -1, 1, 2};
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int t = sc.nextInt();
+
+        int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
-            int n = sc.nextInt();
-            int sx = sc.nextInt(), sy = sc.nextInt();
-            int ex = sc.nextInt(), ey = sc.nextInt();
+            int i = Integer.parseInt(br.readLine());
+            String[] line1 = br.readLine().split(" ");
+            String[] line2 = br.readLine().split(" ");
 
-            int[][] b = new int[n][n];
+            int sx = Integer.parseInt(line1[0]), sy = Integer.parseInt(line1[1]);
+            int ex = Integer.parseInt(line2[0]), ey = Integer.parseInt(line2[1]);
+            
+            int[][] cnt = new int[i][i];
 
-            for (int i=0; i<n; i++){
-                for (int j=0; j<n; j++) {
-                    b[i][j] = -1;
-                }
+            for (int j=0; j<i; j++) {
+                Arrays.fill(cnt[j], -1);
             }
 
             Queue<Pair> q = new LinkedList<>();
             q.add(new Pair(sx, sy));
-            b[sx][sy] = 0;
+            cnt[sx][sy] = 0;
 
             while (!q.isEmpty()) {
                 Pair p = q.remove();
                 int x = p.x, y = p.y;
 
-                if (x == ex && y == ey){
-                    sb.append(b[ex][ey]).append("\n");
+                if (x == ex && y == ey) {
+                    sb.append(cnt[ex][ey]).append("\n");
                     break;
                 }
 
                 for (int k=0; k<8; k++) {
                     int nx = x+dx[k], ny = y+dy[k];
 
-                    if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-                        if (b[nx][ny] == -1){
+                    if (0 <= nx && nx < i && 0 <= ny && ny < i) {
+                        if (cnt[nx][ny] == -1) {
+                            cnt[nx][ny] = cnt[x][y]+1;
                             q.add(new Pair(nx, ny));
-                            b[nx][ny] = b[x][y] + 1;
                         }
                     }
                 }
             }
         }
         System.out.println(sb);
-
     }
 }
