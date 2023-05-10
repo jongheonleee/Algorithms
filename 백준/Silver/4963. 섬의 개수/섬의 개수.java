919fe90cb4c49a68024fb91cd6ec9e08a8993f55
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-10 11:04:34 +0900

    [Silver II] Title: 섬의 개수, Time: 172 ms, Memory: 16928 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -2,40 +2,24 @@ import java.util.*;
 import java.io.*;
 
 
-class Pair {
-    int x, y;
-
-    Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 public class Main {
 
-    public static final int[] dx = {0, 0, 1, -1, 1, -1, 1, -1};
-    public static final int[] dy = {1, -1, 0, 0, -1, 1, 1, -1};
-    public static void bfs(int[][] a, int[][] g, int x, int y, int cnt, int n, int m) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(x, y));
-        g[x][y] = cnt;
+    private static final int[] dx = {0, 0, 1, -1, 1, 1, -1, -1};
+    private static final int[] dy = {1, -1, 0, 0, 1, -1, 1, -1};
 
-        while (!q.isEmpty()) {
-            Pair p = q.remove();
-            x = p.x; y = p.y;
+    public static void dfs(int[][] a, boolean[][] check, int x, int y, int h, int w) {
+        check[x][y] = true;
 
-            for (int k=0; k<8; k++) {
-                int nx = x+dx[k], ny = y+dy[k];
+        for (int k=0; k<8; k++) {
+            int nx = x+dx[k], ny = y+dy[k];
 
-                if (0 <= nx && nx < m && 0 <= ny && ny < n) {
-                    if (a[nx][ny] == 1 && g[nx][ny] == 0) {
-                        q.add(new Pair(nx, ny));
-                        g[nx][ny] = cnt;
-                    }
+            if (0 <= nx && nx < h && 0 <= ny && ny < w) {
+                if (a[nx][ny] == 1 && check[nx][ny] == false) {
+                    dfs(a, check, nx, ny, h, w);
                 }
             }
         }
     }
-
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,30 +27,30 @@ public class Main {
 
         while (true) {
             String[] line1 = br.readLine().split(" ");
-            int n = Integer.parseInt(line1[0]), m = Integer.parseInt(line1[1]);
+            int w = Integer.parseInt(line1[0]), h = Integer.parseInt(line1[1]);
 
-            if (n == 0 && m == 0) break;
+            if (w == 0 && h == 0) break;
 
-            int[][] a = new int[m][n];
-            for (int i=0; i<m; i++) {
+            int[][] a = new int[h][w];
+
+            for (int i=0; i<h; i++) {
                 String[] line2 = br.readLine().split(" ");
-                for (int j=0; j<n; j++) {
+                for (int j=0; j<w; j++) {
                     a[i][j] = Integer.parseInt(line2[j]);
                 }
             }
-
+            boolean[][] check = new boolean[h][w];
             int cnt = 0;
-            int[][] g = new int[m][n];
-            for (int i=0; i<m; i++) {
-                for (int j=0; j<n; j++) {
-                    if (a[i][j] == 1 && g[i][j] == 0) {
-                        bfs(a, g, i, j, ++cnt, n, m);
+            for (int i=0; i<h; i++) {
+                for (int j=0; j<w; j++) {
+                    if (a[i][j] == 1 && check[i][j] == false) {
+                        cnt++;
+                        dfs(a, check, i, j, h, w);
                     }
                 }
             }
             sb.append(cnt).append("\n");
         }
         System.out.println(sb);
-
     }
 }
