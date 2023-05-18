7ec041be40dcc3f1692988eaceeee7ee6dbbc731
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-19 00:01:16 +0900

    [Gold IV] Title: 알고스팟, Time: 136 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
 
 class Pair {
     int x, y;
@@ -13,54 +10,55 @@ class Pair {
     }
 }
 public class Main {
-
+    
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line1 = br.readLine().split(" ");
         int m = Integer.parseInt(line1[0]), n = Integer.parseInt(line1[1]);
+
         int[][] a = new int[n][m];
 
         for (int i=0; i<n; i++) {
             String line2 = br.readLine();
-
             for (int j=0; j<m; j++) {
                 a[i][j] = line2.charAt(j)-'0';
             }
         }
-        int[][] dist = new int[n][m];
-        ArrayDeque<Pair> deq = new ArrayDeque<>();
-        deq.add(new Pair(0, 0));
-
+        int[][] d = new int[n][m];
         for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                dist[i][j] = -1;
-            }
+            Arrays.fill(d[i], -1);
         }
 
-        dist[0][0] = 0;
-        while(!deq.isEmpty()) {
-            Pair p = deq.poll();
-            int x = p.x, y = p.y;
 
+        d[0][0] = 0;
+        ArrayDeque<Pair> deque = new ArrayDeque<>();
+        deque.add(new Pair(0, 0));
+        
+        while (!deque.isEmpty()) {
+            Pair p = deque.poll();
+            int x = p.x, y = p.y;
+            
             for (int k=0; k<4; k++) {
                 int nx = x+dx[k], ny = y+dy[k];
-
+                
                 if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (dist[nx][ny] == -1) {
+                    if (d[nx][ny] == -1) {
                         if (a[nx][ny] == 0) {
-                            dist[nx][ny] = dist[x][y];
-                            deq.addFirst(new Pair(nx, ny));
+                            d[nx][ny] = d[x][y];
+                            deque.addFirst(new Pair(nx, ny));
                         }
                         else {
-                            dist[nx][ny] = dist[x][y] + 1;
-                            deq.addLast(new Pair(nx, ny));
+                            d[nx][ny] = d[x][y]+1;
+                            deque.addLast(new Pair(nx, ny));
                         }
                     }
                 }
             }
         }
-        System.out.println(dist[n-1][m-1]);
+        System.out.println(d[n-1][m-1]);
+
     }
 }
