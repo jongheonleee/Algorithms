d737e80053c6ee2c61facc6a8f3615b96ae6d3ce
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-20 22:39:12 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 268 ms, Memory: 22464 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.java
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.java
@@ -1,63 +1,110 @@
 import java.util.*;
+import java.io.*;
+
 class Node {
-    int left, right;
+    int left;
+    int right;
+
     public int order, depth;
+
     Node(int left, int right) {
         this.left = left;
         this.right = right;
     }
 }
+
 public class Main {
-    static Node[] a = new Node[10001];
-    static int[] left = new int[10001];
-    static int[] right = new int[10001];
-    static int[] cnt = new int[10001];
+
+    static final int LIMIT = 10000;
+
+    static Node[] a = new Node[LIMIT+1];
+
+    static int[] cnt = new int[LIMIT+1];
+
     static int order = 0;
-    static void inorder(int node, int depth) {
-        if (node == -1) return;
-        inorder(a[node].left, depth+1);
-        a[node].order = ++order;
-        a[node].depth = depth;
-        inorder(a[node].right, depth+1);
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i=0; i<n; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int z = sc.nextInt();
-            a[x] = new Node(y, z);
-            if (y != -1) cnt[y] += 1;
-            if (z != -1) cnt[z] += 1;
+
+    static int[] left = new int[LIMIT+1];
+
+    static int[] right = new int[LIMIT+1];
+
+    // L -> N -> R
+    static void inorder(int x, int depth) {
+        if (x == -1) {
+            return;
         }
-        int root = 0;
-        for (int i=1; i<=n; i++) {
-            if (cnt[i] == 0) {
-                root = i;
+
+        inorder(a[x].left, depth+1);
+        a[x].order = ++order;
+        a[x].depth = depth;
+        inorder(a[x].right, depth+1);
+    }
+
+
+
+    public static void main(String args[]) throws IOException {
+        // 트리 구현하기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<n; i++) {
+            String[] line = br.readLine().split(" ");
+            int x = Integer.parseInt(line[0]);
+            int y = Integer.parseInt(line[1]);
+            int z = Integer.parseInt(line[2]);
+
+            a[x] = new Node(y, z);
+
+            if (y != -1) {
+                cnt[y] += 1;
+            }
+
+            if (z != -1) {
+                cnt[z] += 1;
             }
         }
-        inorder(root, 1);
-        int maxdepth = 0;
+        // 루트 찾기(조상이 없는 것이 루트)
+        int loot = 0;
+        for (int i=1; i<=n; i++) {
+            if (cnt[i] == 0) {
+                loot = i;
+            }
+        }
+
+        // 각 정점에 대해서 깊이와 방문순서 기록해주기(inorder)
+        inorder(loot, 1);
+
+        // left, right 채우기 -> 깊이 depth를 기준으로 가장 최근에 방문한 순서 left, 나중에 방문한 순서 right
         for (int i=1; i<=n; i++) {
             int depth = a[i].depth;
             int order = a[i].order;
+
             if (left[depth] == 0) {
                 left[depth] = order;
-            } else {
+            }
+            else {
                 left[depth] = Math.min(left[depth], order);
             }
             right[depth] = Math.max(right[depth], order);
-            maxdepth = Math.max(maxdepth, depth);
         }
-        int ans = 0;
-        int ans_level = 0;
-        for (int i=1; i<=maxdepth; i++) {
-            if (ans < right[i]-left[i]+1) {
-                ans = right[i]-left[i]+1;
-                ans_level = i;
+
+
+        // 최대 깊이 찾기
+        int maxDepth = 0;
+        for (int i=1; i<=n; i++) {
+            if (a[i].depth > maxDepth) {
+                maxDepth = a[i].depth;
             }
         }
-        System.out.println(ans_level + " " + ans);
+
+        int ans = 0;
+        int ansDepth = 0;
+        for (int i=1; i<=maxDepth; i++) {
+            if (ans < right[i] - left[i] + 1) {
+                ans = right[i] - left[i] + 1;
+                ansDepth = i;
+            }
+        }
+        System.out.println(ansDepth + " " + ans);
+
     }
 }
