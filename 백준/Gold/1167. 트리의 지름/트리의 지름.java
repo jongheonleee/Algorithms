3a0a19c163e4c7418fba0f77b6570ff8b4f9e2a4
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-22 16:57:23 +0900

    [Gold II] Title: 트리의 지름, Time: 1784 ms, Memory: 324096 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,63 +1,76 @@
 import java.util.*;
+import java.io.*;
+
 class Edge {
-    public int to;
-    public int cost;
+    int to;
+    int cost;
+
     Edge(int to, int cost) {
         this.to = to;
         this.cost = cost;
     }
 }
 
-
 public class Main {
-    public static int[] bfs(int n, List<Edge>[] a, int start) {
-        boolean[] check = new boolean[n+1];
+
+    static int[] bfs(int n, List<Edge>[] a, int start) {
+        Queue<Integer> q = new LinkedList<>();
         int[] dist = new int[n+1];
-        Queue<Integer> q = new LinkedList<Integer>();
-        check[start] = true;
-        q.add(start);
+        boolean[] check = new boolean[n+1];
+        q.add(start); dist[start] = 0; check[start] = true;
+
         while (!q.isEmpty()) {
             int x = q.remove();
             for (Edge e : a[x]) {
                 int y = e.to;
                 int cost = e.cost;
+
                 if (check[y] == false) {
                     dist[y] = dist[x] + cost;
-                    q.add(y);
                     check[y] = true;
+                    q.add(y);
                 }
             }
         }
+
         return dist;
     }
-    
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Edge>[] a = (List<Edge>[]) new List[n+1];
+        List<Edge>[] a = (List<Edge>[])new List[n+1];
+
         for (int i=1; i<=n; i++) {
             a[i] = new ArrayList<Edge>();
         }
+
+        // 트리 구현해주기
         for (int i=1; i<=n; i++) {
             int x = sc.nextInt();
             while (true) {
                 int y = sc.nextInt();
                 if (y == -1) break;
                 int z = sc.nextInt();
-                a[x].add(new Edge(y,z));
+
+                a[x].add(new Edge(y, z));
             }
         }
-        int[] dist = bfs(n, a,1);
+
+        // bfs 탐색 2번 진행해주기
+        int[] dist = bfs(n, a, 1);
         int start = 1;
         for (int i=2; i<=n; i++) {
-            if (dist[i] > dist[start]) {
+            if (dist[start] < dist[i]) {
                 start = i;
             }
         }
+
         dist = bfs(n, a, start);
-        int ans = dist[1];
-        for (int i=2; i<=n; i++) {
-            if (ans < dist[i]) {
+
+        // 최대 길이(지름) 찾기
+        int ans = 0;
+        for (int i=1; i<=n; i++) {
+            if (dist[i] > ans) {
                 ans = dist[i];
             }
         }
