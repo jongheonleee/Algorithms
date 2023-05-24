53351b1c4355bd64a0925abad3d90fa131ad00a8
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-24 10:26:09 +0900

    [Silver III] Title: 다음 순열, Time: 168 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.java
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -1,45 +1,62 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-    
+import java.io.*;
 
-    public static boolean next_permutation(int[] a) {
+public class Main {
+
+    static int[] a;
+
+    static void swap(int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j]; a[j] = tmp;
+    }
+
+    static void reverse(int i) {
+        int j = a.length-1;
+
+        while (i < j) {
+            int tmp = a[i];
+            a[i] = a[j]; a[j] = tmp;
+            i++; j--;
+        }
+    }
+
+    static boolean next_permutation() {
         int i = a.length-1;
-        while (i>0 && a[i-1] >= a[i]) i--;
+        while (i > 0 && a[i-1] >= a[i])
+            i--;
 
         if (i <= 0) return false;
 
         int j = a.length-1;
-        while (a[i-1] >= a[j]) j--;
+        while (j > i && a[i-1] >= a[j])
+            j--;
 
-        int tmp = a[i-1];
-        a[i-1] = a[j]; a[j] = tmp;
-
-        j = a.length-1;
-        while(i < j) {
-            tmp = a[i];
-            a[i] = a[j]; a[j] = tmp;
-            i++; j--;
-        }
+        swap(i-1, j);
+        reverse(i);
 
         return true;
     }
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
+        a = new int[n];
+
         String[] line = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
             a[i] = Integer.parseInt(line[i]);
         }
-        if (next_permutation(a)) {
+
+        if (next_permutation()) {
             for (int i=0; i<n; i++) {
                 sb.append(a[i]).append(" ");
             }
-            System.out.println(sb);
-        }else {
-            System.out.println(-1);
+        } else {
+            sb.append(-1);
         }
+
+        System.out.println(sb);
+
     }
 }
