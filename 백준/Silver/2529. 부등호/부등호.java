6ed1adc29edc4e684ceb6c2b766e7490f9468589
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-24 15:27:52 +0900

    [Silver I] Title: 부등호, Time: 192 ms, Memory: 15440 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,55 +1,124 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
 
-    static int n;
-    static char[] a;
-    static boolean[] check = new boolean[10];
-    static ArrayList<String> result = new ArrayList<>();
 
-    static boolean good(char x, char y, char op) {
-        if (op == '>') {
-            if (x < y) return false;
-        }
-        else {
-            if (x > y) return false;
+    static boolean next_permutation(int[] a) {
+        int i = a.length-1;
+        while (i > 0 && a[i-1] >= a[i])
+            i--;
+
+        if (i <= 0) return false;
+
+        int j = a.length-1;
+        while (j > i && a[i-1] >= a[j])
+            j--;
+
+        // swap
+        int tmp = a[i-1];
+        a[i-1] = a[j]; a[j] = tmp;
+
+        // reverse
+        j=a.length-1;
+        while (i < j) {
+            tmp = a[i];
+            a[i] = a[j]; a[j] = tmp;
+
+            i++; j--;
         }
 
         return true;
     }
 
-    static void go(int idx, String num) {
-        // 정답을 찾은 경우
-        if (idx == n+1) {
-            result.add(num);
-            return;
-        }
-
-        for (int i=0; i<=9; i++) {
-            if (check[i]) continue;
-
-            if (idx == 0 || good(num.charAt(idx-1), (char)(i+'0'), a[idx-1])) {
-                check[i] = true;
-                go(idx+1, num + Integer.toString(i));
-                check[i] = false;
+    static boolean check(int[] p, char[] a) {
+        for (int i=0; i<a.length; i++) {
+            if (a[i] == '<' && p[i] > p[i+1]) {
+                return false;
+            }
+            if (a[i] == '>' && p[i] < p[i+1]) {
+                return false;
             }
         }
+
+        return true;
     }
 
+    static boolean prev_permutation(int[] a) {
+        int i = a.length-1;
+        while (i > 0 && a[i-1] <= a[i])
+            i--;
+
+        if (i <= 0) return false;
+
+        int j = a.length-1;
+        while (j > i && a[i-1] <= a[j])
+            j--;
+
+        // swap
+        int tmp = a[i-1];
+        a[i-1] = a[j]; a[j] = tmp;
+
+        // reverse
+        j = a.length-1;
+        while (i < j) {
+            tmp = a[i];
+            a[i] = a[j]; a[j] = tmp;
+
+            i++; j--;
+        }
+
+        return true;
+    }
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        a = new char[n];
+        StringBuilder sb = new StringBuilder();
+
+        int k = Integer.parseInt(br.readLine());
+        char[] a = new char[k];
+
         String[] line = br.readLine().split(" ");
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<k; i++) {
             a[i] = line[i].charAt(0);
         }
 
-        go(0, "");
-        Collections.sort(result);
-        System.out.println(result.get(result.size()-1));
-        System.out.println(result.get(0));
+        int[] small = new int[k+1];
+        int[] big = new int[k+1];
+
+        for (int i=0; i<=k; i++) {
+            small[i] = i;
+            big[i] = 9-i;
+        }
+
+        do {
+            if (check(small, a)) {
+                break;
+            }
+        } while (next_permutation(small));
+
+        do {
+            if (check(big, a)) {
+                break;
+            }
+        } while (prev_permutation(big));
+
+        for (int i=0; i<=k; i++) {
+            sb.append(big[i]);
+            if (i == k) {
+                sb.append("\n");
+            }
+        }
+
+
+        for (int i=0; i<=k; i++) {
+            sb.append(small[i]);
+            if (i == k) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
 
     }
 }
