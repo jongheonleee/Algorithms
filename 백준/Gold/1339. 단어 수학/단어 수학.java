44411d66009862b58d7c7686579b8ced63f1c74a
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-24 20:01:41 +0900

    [Gold IV] Title: 단어 수학, Time: 1012 ms, Memory: 296916 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,75 +1,90 @@
 import java.util.*;
+import java.io.*;
 public class Main {
+
+    static final int LIMIT = 256;
+
+    static int[] alphaToNum = new int[LIMIT];
+
     static boolean next_permutation(int[] a) {
         int i = a.length-1;
-        while (i > 0 && a[i-1] >= a[i]) {
-            i -= 1;
-        }
+        while (i > 0 && a[i] <= a[i-1])
+            i--;
 
-        if (i <= 0) {
-            return false;
-        }
+        if (i <= 0) return false;
 
         int j = a.length-1;
-        while (a[j] <= a[i-1]) {
-            j -= 1;
-        }
+        while (j > i && a[j] <= a[i-1])
+            j--;
 
-        int temp = a[i-1];
-        a[i-1] = a[j];
-        a[j] = temp;
+        int tmp = a[i-1];
+        a[i-1] = a[j]; a[j] = tmp;
 
         j = a.length-1;
         while (i < j) {
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-            i += 1;
-            j -= 1;
+            tmp = a[i];
+            a[i] = a[j]; a[j] = tmp;
+
+            i++; j--;
         }
+
         return true;
+
     }
-    static int[] alpha = new int[256];
-    static int calc(String[] a, Character[] letters, int[] d) {
+
+    static int calc(String[] words, Character[] letters, int[] num) {
         int m = letters.length;
         int sum = 0;
+
         for (int i=0; i<m; i++) {
-            alpha[letters[i]] = d[i];
+            char alphabet = letters[i];
+            alphaToNum[alphabet] = num[i];
         }
-        for (String s : a) {
+
+        for (String word : words) {
             int now = 0;
-            for (char x : s.toCharArray()) {
-                now = now * 10 + alpha[x];
+            for (char alphabet : word.toCharArray()) {
+                now = now*10 + alphaToNum[alphabet];
             }
+
             sum += now;
         }
+
         return sum;
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] a = new String[n];
-        HashSet<Character> s = new HashSet<>();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] words = new String[n];
+        HashSet<Character> set = new HashSet<>();
+
         for (int i=0; i<n; i++) {
-            a[i] = sc.next();
-            for (char x : a[i].toCharArray()) {
-                s.add(x);
+            words[i] = br.readLine();
+            String word = words[i];
+            for (char alphabet : word.toCharArray()) {
+                set.add(alphabet);
             }
         }
-        Character[] letters = s.toArray(new Character[s.size()]);
+
+        Character[] letters = set.toArray(new Character[set.size()]);
+
         int m = letters.length;
-        int[] d = new int[m];
+        int[] num = new int[m];
+
         for (int i=0; i<m; i++) {
-            d[i] = 9-i;
+            num[i] = 9-i;
         }
-        Arrays.sort(d);
+
+        Arrays.sort(num);
         int ans = 0;
+        
         do {
-            int now = calc(a, letters, d);
-            if (ans < now) {
-                ans = now;
+            int res = calc(words, letters, num);
+            if (res > ans) {
+                ans = res;
             }
-        } while(next_permutation(d));
+        } while (next_permutation(num));
+
         System.out.println(ans);
     }
 }
