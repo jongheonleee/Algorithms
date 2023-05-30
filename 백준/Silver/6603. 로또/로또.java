7b4c7eaf10cefcb8003555dc63200536ef1337eb
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-30 11:09:05 +0900

    [Silver II] Title: 로또, Time: 228 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,81 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main {
-
+    static final int COUNT = 6;
     static StringBuilder sb = new StringBuilder();
 
-    static boolean next_permutation(int[] a) {
-        int i = a.length-1;
-        while (i>0 && a[i]<=a[i-1]) i--;
-
-        if (i<=0) return false;
-
-        int j = a.length-1;
-        while (a[i-1]>=a[j]) j--;
-        int tmp = a[i-1];
-        a[i-1]=a[j]; a[j]=tmp;
-
-        j = a.length-1;
-        while (i<j) {
-            tmp = a[j];
-            a[j]=a[i]; a[i]=tmp;
-            i++; j--;
+    static ArrayList<Integer> lotto = new ArrayList<>();
+    static void go(int[] a, int idx, int cnt) {
+        if (cnt == COUNT) {
+            for (int n : lotto) {
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
         }
-        return true;
-    }
 
+        int n = a.length;
+        if (n == idx) return;
+
+        lotto.add(a[idx]);
+        go(a, idx+1, cnt+1);
+        lotto.remove(lotto.size()-1);
+        go(a, idx+1, cnt);
+
+    }
     public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
+
         while (true) {
-            int n = sc.nextInt();
-            if (n==0) break;
-            int[] a = new int[n];
-            for (int i=0; i<n; i++) a[i] = sc.nextInt();
-
-            int[] d = new int[n];
-            for (int i=0; i<n; i++) {
-                if (i<n-6) d[i] = 0;
-                else d[i] = 1;
+            int k = sc.nextInt();
+            if (k == 0) break;
+            int[] a = new int[k];
+            for (int i=0; i<k; i++) {
+                a[i] = sc.nextInt();
             }
 
-            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-
-            do {
-                ArrayList<Integer> cur = new ArrayList<>();
-                for (int i=0; i<n; i++) {
-                    if (d[i] == 1) {
-                        cur.add(a[i]);
-                    }
-                }
-                ans.add(cur);
-            } while(next_permutation(d));
-
-            Collections.sort(ans, new Comparator<ArrayList<Integer>>() {
-                @Override
-                public int compare(ArrayList<Integer> l1, ArrayList<Integer> l2) {
-                    int n = l1.size(), m = l2.size(), i=0;
-                    while (i<n && i<m) {
-                        int t1 = l1.get(i), t2 = l2.get(i);
-
-                        if (t1 < t2) return -1;
-                        else if(t1 > t2) return 1;
-                        i++;
-                    }
-                    if (i==n && i!=m) return -1;
-                    else if (i!=n && i==m) return 1;
-                    return 0;
-                }
-            });
-
-            for (ArrayList<Integer> v : ans) {
-                for (int x : v) {
-                    sb.append(x).append(" ");
-                }
-                sb.append("\n");
-            }
+            go(a, 0, 0);
             sb.append("\n");
         }
 
         System.out.println(sb);
+
     }
 }
