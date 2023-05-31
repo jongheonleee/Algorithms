e905eb85dc8a954d1409ddc8e6b59d77525b47a7
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-05-31 15:26:34 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 172 ms, Memory: 16288 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,74 +1,88 @@
 import java.util.*;
+import java.io.*;
 public class Main {
+
+    static int max = -1-000-000-000;
+    static int min = 1-000-000-000;
+
+    static int calculate(int[] num, int[] a) {
+        int result = num[0];
+
+        for (int i=1; i<num.length; i++) {
+            int kind = a[i-1];
+            if (kind == 0) {
+                result += num[i];
+            } else if (kind == 1) {
+                result -= num[i];
+            } else if (kind == 2) {
+                result *= num[i];
+            } else {
+                result /= num[i];
+            }
+        }
+
+
+        return result;
+    }
+
     static boolean next_permutation(int[] a) {
         int i = a.length-1;
-        while (i > 0 && a[i-1] >= a[i]) {
-            i -= 1;
-        }
+        while (i > 0 && a[i-1] >= a[i]) i--;
 
-        if (i <= 0) {
-            return false;
-        }
+        if (i <= 0) return false;
 
         int j = a.length-1;
-        while (a[j] <= a[i-1]) {
-            j -= 1;
-        }
+        while ( j > i && a[i-1] >= a[j]) j--;
 
-        int temp = a[i-1];
-        a[i-1] = a[j];
-        a[j] = temp;
+        int tmp = a[i-1];
+        a[i-1] = a[j]; a[j] = tmp;
 
         j = a.length-1;
-        while (i < j) {
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-            i += 1;
-            j -= 1;
+        while (j > i) {
+            tmp = a[i];
+            a[i] = a[j]; a[j] = tmp;
+
+            i++; j--;
         }
+
         return true;
     }
-    static int calc(int[] a, int[] b) {
-        int n = a.length;
-        int ans = a[0];
-        for (int i=1; i<n; i++) {
-            if (b[i-1] == 0) {
-                ans = ans + a[i];
-            } else if (b[i-1] == 1) {
-                ans = ans - a[i];
-            } else if (b[i-1] == 2) {
-                ans = ans * a[i];
-            } else {
-                ans = ans / a[i];
-            }
-        }
-        return ans;
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 수열 입력받기
+        int[] num = new int[n];
+        String[] line1 = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
+            num[i] = Integer.parseInt(line1[i]);
         }
-        int[] b = new int[n-1];
-        int m = 0;
-        for (int i=0; i<4; i++) {
-            int cnt = sc.nextInt();
+
+
+        // 1. 각 연산자의 개수 입력받기 -> 0:+, 1:-, 2:*, 3:/
+        // 2. 연산자 조합을 수열로 나타내기 -> (예시) ++- : 001
+        int[] a = new int[n-1];
+        String[] line2 = br.readLine().split(" ");
+        for (int i=0, j=0; i<4; i++) {
+            int cnt = Integer.parseInt(line2[i]);
             for (int k=0; k<cnt; k++) {
-                b[m++] = i;
+                a[j] = i;
+                j++;
             }
         }
-        Arrays.sort(b);
-        ArrayList<Integer> result = new ArrayList<>();
+
+        Arrays.sort(a);
+
+        ArrayList<Integer> store = new ArrayList<>();
         do {
-            int temp = calc(a, b);
-            result.add(temp);
-        } while(next_permutation(b));
-        Collections.sort(result);
-        m = result.size();
-        System.out.println(result.get(m-1));
-        System.out.println(result.get(0));
+            int result = calculate(num, a);
+            store.add(result);
+        } while (next_permutation(a));
+
+        Collections.sort(store);
+        System.out.println(store.get(store.size()-1));
+        System.out.println(store.get(0));
+
     }
 }
