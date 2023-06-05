4bd741b888df4d9cf7990d0aa165217dd4137212
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-05 15:38:35 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,35 +1,40 @@
 import java.io.*;
-
+import java.util.*;
 public class Main {
-    static int limit = 15;
-    static int[] t = new int[limit+1];
-    static int[] p = new int[limit+1];
-    static int result = 0;
 
+    static int n;
+    static int[][] a;
+    static int ans = 0;
 
-    public static void go(int day, int price, int n) {
+    static void go(int day, int price) {
+        // 종료 조건
         if (day == n+1) {
-            result = result < price ? price : result;
+            if (ans < price)
+                ans = price;
             return;
         }
 
-        if (day > n+1) return;
+        if (day > n+1)
+            return;
 
-        go(day+t[day], price+p[day], n);
-        go(day+1, price, n);
+        go(day+1, price);
+        go(day+a[0][day], price+a[1][day]);
+
     }
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        a = new int[n+1][n+1];
+
         for (int i=1; i<=n; i++) {
             String[] line = br.readLine().split(" ");
-            t[i] = Integer.parseInt(line[0]);
-            p[i] = Integer.parseInt(line[1]);
+            a[0][i] = Integer.parseInt(line[0]);
+            a[1][i] = Integer.parseInt(line[1]);
         }
 
-        go(1, 0, n);
-        System.out.println(result);
-
+        go(1, 0);
+        System.out.println(ans);
     }
 }
