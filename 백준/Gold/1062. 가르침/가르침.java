fd98adc5b8d35936b10a2c6b60f2689346695ace
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-06 20:58:19 +0900

    [Gold IV] Title: 가르침, Time: 244 ms, Memory: 15640 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,57 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class Main {
+    static final int alphabets = (1<<26)-1;
 
-    static final int alphabets = 26;
-    static boolean[] learn = new boolean[alphabets];
-    static int count(String[] words) {
-        int cnt = 0;
-        for (String word : words) {
-            boolean ok = true;
-            for (char x : word.toCharArray()) {
-                if (!learn[x-'a']) {
-                    ok = false;
-                    break;
-                }
+    static int[] words;
+
+    static int go(int i, int k, int mask) {
+        // 종료조건
+        if (k < 0) {
+            return 0;
+        }
+
+        if (i == 26) {
+            int cnt = counter(mask);
+            return cnt;
+        }
+        int res = 0;
+        // i 번째 글자를 사용하는 경우
+        int tmp = go(i+1, k-1, mask | (1<<i));
+
+        if (res < tmp) {
+            res = tmp;
+        }
+        // i 번째 글자를 사용하지 않는 경우
+        if (i != 'a'-'a' && i != 'n'-'a' && i != 't'-'a'
+                && i != 'i'-'a' && i != 'c'-'a') {
+            tmp = go(i+1, k, mask);
+
+            if (res < tmp) {
+                res = tmp;
             }
-            if (ok) cnt += 1;
         }
-        return cnt;
+
+        return res;
     }
-    
-    // k개의 글자를 배우는 모든 경우(불필요한 경우 제외)를 구해보고 그때의 읽을 수 있는 단어를 카운팅해줌
-    static int go(int index, int k, String[] words) {
-        if (k < 0) return 0;
-        if (index == 26) {
-            return count(words);
-        }
 
-        int ans = 0;
-        // index 번째 글자를 배운 경우
-        learn[index] = true;
-        int t1 = go(index+1, k-1, words);
-        learn[index] = false;
-        if (ans < t1) ans = t1;
+    static int counter(int mask) {
+        int cnt = 0;
+        int notTaughtAlphabets = alphabets - mask;
 
-        // 배우지 않는 경우
-        // 밑에 조건문은 해당 글자들은 반드시 배워야 하는 글자이므로 해당 글자일 경우 무조건 배우게함
-        if (index != 'a'-'a' && index != 'n'-'a' && index != 't'-'a' && index != 'i'-'a' && index != 'c'-'a') {
-            t1 = go(index+1, k, words);
-            if (ans < t1) ans = t1;
+        for (int word : words) {
+            if ((word & notTaughtAlphabets) == 0) {
+                cnt++;
+            }
         }
         
-        // 읽을 수 있는 단어의 최대 개수 반환
-        return ans;
+        return cnt;
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
 
-        String[] words = new String[n];
+        int n = Integer.parseInt(line[0]), k = Integer.parseInt(line[1]);
+        words = new int[n];
+
         for (int i=0; i<n; i++) {
-            words[i] = sc.next();
+            String word = br.readLine();
+            for (char alphabet : word.toCharArray()) {
+                words[i] |= (1 << alphabet - 'a');
+            }
         }
 
-        System.out.println(go(0,m,words));
+        int ans = go(0, k, 0);
+        System.out.println(ans);
     }
 }
