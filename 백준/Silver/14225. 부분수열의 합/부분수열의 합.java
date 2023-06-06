806b63227114515d04b1790f6ad0d8c21af64c9a
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-06 15:20:05 +0900

    [Silver I] Title: 부분수열의 합, Time: 140 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -1,42 +1,36 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static int LIMIT = 2000000;
-
-    static boolean[] check = new boolean[LIMIT+1];
-
-    static void go(int[] a, int idx, int sum) {
-        if (idx == a.length) {
+    static int n;
+    static int[] a;
+    static boolean[] check = new boolean[20 * 100000 + 10];
+    static void go(int i, int sum) {
+        if (i == n) {
             check[sum] = true;
             return;
         }
 
-        go(a, idx+1, sum+a[idx]);
-        go(a, idx+1, sum);
-
+        go(i+1, sum+a[i]);
+        go(i+1, sum);
     }
 
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line1 = br.readLine().split(" ");
-        int n = Integer.parseInt(line1[0]);
-        int[] a = new int[n];
+        n = Integer.parseInt(br.readLine());
+        a = new int[n];
 
-        String[] line2 = br.readLine().split(" ");
+        String[] line = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            a[i] = Integer.parseInt(line2[i]);
+            a[i] = Integer.parseInt(line[i]);
         }
 
+        go(0, 0);
 
-        go(a, 0, 0);
-
-        for (int i=1; i<=LIMIT; i++) {
-            if (check[i] == false) {
+        for (int i=1;;i++) {
+            if (!check[i]) {
                 System.out.println(i);
                 break;
             }
         }
-
     }
 }
