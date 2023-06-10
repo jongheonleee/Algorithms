a5eb30d3fb76d64da3fb1f4cf27983b414f8ce98
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-10 22:23:47 +0900

    [Gold IV] Title: 돌 그룹, Time: 320 ms, Memory: 144412 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -1,38 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class Main {
-    public static int sum = 0;
-    public static boolean[][] check = new boolean[1501][1501];
-    public static void go(int x, int y) {
-        if (check[x][y]) return;
+
+    static final int LIMIT = 1500;
+
+    static boolean[][] check = new boolean[LIMIT+1][LIMIT+1];
+    
+    static int sum = 0;
+
+    static void go(int x, int y) {
+        if (check[x][y] == true) {
+            return;
+        }
+        
         check[x][y] = true;
-        int[] a = {x, y, sum-x-y};
+        int[] rocks = {x, y, sum-(x+y)};
+        
         for (int i=0; i<3; i++) {
             for (int j=0; j<3; j++) {
-                if (a[i] < a[j]) {
-                    int[] b = {x, y, sum-x-y};
-                    b[i] += a[i];
-                    b[j] -= a[i];
-                    go(b[0], b[1]);
+                if (rocks[i] < rocks[j]) {
+                    int[] tmp = {x, y, sum-(x+y)};
+                    tmp[i] += rocks[i];
+                    tmp[j] -= rocks[i];
+                    go(tmp[0], tmp[1]);
                 }
             }
         }
-
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int z = sc.nextInt();
-        sum = x+y+z;
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+
+        int a = Integer.parseInt(line[0]);
+        int b = Integer.parseInt(line[1]);
+        int c = Integer.parseInt(line[2]);
+
+        sum = a + b + c;
         if (sum % 3 != 0) {
             System.out.println(0);
             System.exit(0);
         }
-        go(x, y);
-        if (check[sum/3][sum/3]) {
+
+        go(a, b);
+        
+        if (check[sum/3][sum/3] == true) {
             System.out.println(1);
         } else {
             System.out.println(0);
         }
+        System.exit(0);
+
     }
 }
