405b3b49cae9ba42438a23ae14845866ccb12b24
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-10 22:56:18 +0900

    [Gold IV] Title: 돌 그룹, Time: 304 ms, Memory: 142704 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -1,23 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 public class Main {
 
-    static final int LIMIT = 1500;
+    static int sum;
 
-    static boolean[][] check = new boolean[LIMIT+1][LIMIT+1];
-    
-    static int sum = 0;
+    static final int limit = 1500;
 
+    static boolean[][] check = new boolean[limit+1][limit+1];
     static void go(int x, int y) {
         if (check[x][y] == true) {
             return;
         }
-        
+
         check[x][y] = true;
         int[] rocks = {x, y, sum-(x+y)};
-        
+
         for (int i=0; i<3; i++) {
             for (int j=0; j<3; j++) {
                 if (rocks[i] < rocks[j]) {
@@ -29,6 +26,7 @@ public class Main {
             }
         }
     }
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line = br.readLine().split(" ");
@@ -37,20 +35,18 @@ public class Main {
         int b = Integer.parseInt(line[1]);
         int c = Integer.parseInt(line[2]);
 
-        sum = a + b + c;
+        sum = a+b+c;
         if (sum % 3 != 0) {
             System.out.println(0);
             System.exit(0);
         }
 
         go(a, b);
-        
-        if (check[sum/3][sum/3] == true) {
+
+        if (check[sum/3][sum/3]) {
             System.out.println(1);
         } else {
             System.out.println(0);
         }
-        System.exit(0);
-
     }
 }
