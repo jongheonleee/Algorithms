948fffcd9abf2d4a1274dbe9e64410718583f24a
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-14 19:52:19 +0900

    [Gold III] Title: 아기 상어, Time: 216 ms, Memory: 26492 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,112 +1,152 @@
 import java.util.*;
-class Pair {
-    // 아기 상어가 (x, y)에 있는 해당 물고기를 먹는데 걸린 시간(거리)
-    int dist, x, y;
-    Pair(int dist, int x, int y) {
+import java.io.*;
+
+/**
+ * 더 이상 먹을 수 있는 물고기가 공간에 없다면 아기 상어는 엄마 상어에게 도움을 요청한다.
+ * 먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다. -> dfs/bfs
+ * 먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다. -> bfs
+ * 거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 지나야하는 칸의 개수의 최솟값이다. ->
+ * 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
+ */
+class Fish {
+    int x, y, dist;
+
+    Fish(int x, int y, int dist) {
+        this.x = x;
+        this.y = y;
         this.dist = dist;
+    }
+}
+
+class Pair {
+    int x, y;
+
+    Pair(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
 public class Main {
-    static final int[] dx = {0,0,1,-1};
-    static final int[] dy = {1,-1,0,0};
-    static Pair bfs(int[][] sea, int sx, int sy, int size) {
-        int n = sea.length;
-        ArrayList<Pair> ans = new ArrayList<>();
+
+
+    static int n;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static boolean can(int x, int y, int[][] dist) {
+        return (0 <= x && x < n && 0 <= y && y < n) && dist[x][y] == -1;
+    }
+
+    private static Fish bfs(int[][] sea, int sx, int sy, int size) {
         int[][] dist = new int[n][n];
+
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 dist[i][j] = -1;
             }
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        q.add(sx); q.add(sy);
+        ArrayList<Fish> eatableFishes = new ArrayList<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sx, sy));
         dist[sx][sy] = 0;
-        while (!q.isEmpty()) {
-            int x = q.remove();
-            int y = q.remove();
+
+        while (!(q.isEmpty())) {
+            Pair p = q.remove();
+
+            int x = p.x, y = p.y;
+
             for (int k=0; k<4; k++) {
-                int nx = x+dx[k];
-                int ny = y+dy[k];
-                if (0 <= nx && nx < n && 0 <= ny && ny < n && dist[nx][ny] == -1) {
-                    boolean ok = false;
-                    boolean eat = false;
-                    // 아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없고, 나머지 칸은 모두 지나갈 수 있다.
-                    if (sea[nx][ny] == 0) {
-                        ok = true;
-                    } else if (sea[nx][ny] < size) { // 아기 상어는 자신의 크기보다 작은 물고기만 먹을 수 있다.
-                        ok = eat = true;
-                    } else if (sea[nx][ny] == size) { // 크기가 같은 물고기는 먹을 수 없지만, 그 물고기가 있는 칸은 지나갈 수 있다.
-                        ok = true;
-                    }
-                    if (ok) {
-                        q.add(nx); q.add(ny);
-                        dist[nx][ny] = dist[x][y] + 1;
-                        if (eat) {
-                            ans.add(new Pair(dist[nx][ny], nx, ny));
-                        }
+                int nx = x+dx[k], ny = y+dy[k];
+
+                if (can(nx, ny, dist) == false) continue;
+                boolean move = false;
+                boolean eat = false;
+                // 이동만 가능한 경우
+                if (sea[nx][ny] == 0 || sea[nx][ny] == size) {
+                    move = true;
+                }
+                // 이동할 수 있고 먹을수도 있는 경우
+                else if (sea[nx][ny] < size) {
+                    move = true;
+                    eat = true;
+                }
+
+                if (move) {
+                    q.add(new Pair(nx, ny));
+                    dist[nx][ny] = dist[x][y] + 1;
+
+                    if (eat) {
+                        eatableFishes.add(new Fish(nx, ny, dist[nx][ny]));
                     }
                 }
-            }
-        }
-        if (ans.isEmpty()) {
-            return null;
-        }
-        Pair best = ans.get(0);
 
-        // 먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
-        // 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
-        for (Pair p : ans) {
-            if (best.dist > p.dist) {
-                best = p;
-            } else if (best.dist == p.dist && best.x > p.x) {
-                best = p;
-            } else if (best.dist == p.dist && best.x == p.x && best.y > p.y) {
-                best = p;
             }
         }
-        return best;
+
+        if (eatableFishes.size() == 0) return null;
+
+        Fish eatenFish = eatableFishes.get(0);
+        for (Fish fish : eatableFishes) {
+            if (eatenFish.dist > fish.dist) {
+                eatenFish = fish;
+            } else if (eatenFish.dist == fish.dist && eatenFish.x > fish.x) {
+                eatenFish = fish;
+            } else if (eatenFish.dist == fish.dist && eatenFish.x == fish.x && eatenFish.y > fish.y) {
+                eatenFish = fish;
+            }
+        }
+
+        return eatenFish;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        // 바다 정보(문제에서 제시한 정보) 구현
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
         int[][] sea = new int[n][n];
-        // 아기 상어 위치 저장
-        int x = 0, y = 0;
+
+        for (int i=0; i<n; i++) {
+            String[] input = br.readLine().split(" ");
+
+            for (int j=0; j<n; j++) {
+                sea[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        int babySharkX = 0;
+        int babySharkY = 0;
+        int babySharkSize = 2;
+        int babySharkDist = 0;
+
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                sea[i][j] = sc.nextInt();
                 if (sea[i][j] == 9) {
-                    x = i; y = j;
-                    sea[i][j] = 0;
+                    babySharkX = i;
+                    babySharkY = j;
+                    sea[babySharkX][babySharkY] = 0;
                 }
             }
         }
-        // 아기 상어가 엄마 상어에게 도움을 요청하지 않고
-        // 물고기를 잡아먹을 수 있는 시간을 출력
-        int ans = 0;
-        // 아기 상어의 초기 크기
-        int size = 2;
-        // 물고기 먹은 횟수(아기 상어의 크기 == 물고기 먹은 횟수 -> +1)
-        int exp = 0;
 
+        int eaten = 0;
         while (true) {
-            Pair p = bfs(sea, x, y, size);
-            if (p == null) break;
-            sea[p.x][p.y] = 0;
-            ans += p.dist;
-            exp += 1;
-            if (size == exp) {
-                size += 1;
-                exp = 0;
+            Fish eatenFish = bfs(sea, babySharkX, babySharkY, babySharkSize);
+
+            if (eatenFish == null) break;
+            eaten++;
+            sea[eatenFish.x][eatenFish.y] = 0;
+
+            babySharkX = eatenFish.x;
+            babySharkY = eatenFish.y;
+            babySharkDist += eatenFish.dist;
+
+            if (babySharkSize == eaten) {
+                babySharkSize++;
+                eaten = 0;
             }
-            x = p.x;
-            y = p.y;
         }
-        System.out.println(ans);
+
+        System.out.println(babySharkDist);
     }
 }
