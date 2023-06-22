bee1bcfb47005c6d8d4a4090e2100cb522cb879c
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-22 10:29:00 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,26 +1,29 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] times = new int[n];
+        String[] line = br.readLine().split(" ");
 
-        String[] input = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            times[i] = Integer.parseInt(input[i]);
+            times[i] = Integer.parseInt(line[i]);
         }
 
         Arrays.sort(times);
+        int ans = 0, acc = 0;
 
-        int cumulativeTime = 0;
-        int ans = 0;
         for (int time : times) {
-            cumulativeTime += time;
-            ans += cumulativeTime;
+            acc += time;
+            ans += acc;
         }
+
         System.out.println(ans);
     }
+
 }
