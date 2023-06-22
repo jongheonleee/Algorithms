6afc8d637593b864fc9a684c4e1bdd8ae6382fb9
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-22 16:37:01 +0900

    [Gold I] Title: 동전 뒤집기, Time: 1572 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -1,36 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class Main {
-    static char flip(char x) {
-        if (x == 'H') {
+
+    static char flip(char coin) {
+        if (coin == 'H') {
             return 'T';
-        } else {
+        }
+        else {
             return 'H';
         }
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] a = new String[n];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        char[][] table = new char[n][n];
         for (int i=0; i<n; i++) {
-            a[i] = sc.next();
+            String line = br.readLine();
+            table[i] = line.toCharArray();
         }
+
         int ans = n*n;
         for (int state=0; state<(1<<n); state++) {
             int sum = 0;
+
             for (int j=0; j<n; j++) {
                 int cnt = 0;
+
                 for (int i=0; i<n; i++) {
-                    char cur = a[i].charAt(j);
-                    if ((state & (1 << i)) != 0) {
-                        cur = flip(cur);
+                    char coin = table[i][j];
+                    if (((1<<i) & state) != 0) {
+                        coin = flip(table[i][j]);
                     }
-                    if (cur == 'T') {
-                        cnt += 1;
+
+                    if (coin == 'T') {
+                        cnt++;
                     }
                 }
+
                 sum += Math.min(cnt, n-cnt);
             }
-            if (ans > sum) ans = sum;
+
+            if (sum < ans) {
+                ans = sum;
+            }
         }
         System.out.println(ans);
     }
