b65e762ccb11f5c1beeefed35963721aafd4da4d
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-23 18:38:25 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 1916 ms, Memory: 320540 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -1,29 +1,45 @@
 import java.util.*;
+import java.io.*;
+
+
 public class Main {
-    static int lower_bound(int[] a, int n, int key) {
-        int left = 0;
-        int right = n;
+
+    static int lower_bound(int[] d, int n, int key) {
+        int left = 0, right = n;
+
         while (left < right) {
-            int mid = (left+right)/2;
-            if (key <= a[mid]) {
+            int mid = (left + right) / 2;
+
+            if (d[mid] >= key) {
                 right = mid;
-            } else {
-                left = mid+1;
+            }
+            else {
+                left = mid + 1;
             }
         }
+
         return left;
     }
-    public static void main(String args[]) {
+
+    public static void main(String args[]) throws IOException{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n];
+        int[] d = new int[n];
+
         int len = 0;
         for (int i=0; i<n; i++) {
             int num = sc.nextInt();
-            int p = lower_bound(a, len, num);
-            a[p] = num;
-            if (len == p) len += 1;
+            int idx = lower_bound(d, len, num);
+            if (d[idx] == 0) {
+                d[idx] = num;
+                len++;
+            } else {
+                d[idx] = num;
+            }
         }
+
         System.out.println(len);
+
+
     }
 }
