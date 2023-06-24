c37c704bd57fe465bef2c9c5e20065e3d3fa5e6c
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-24 21:15:44 +0900

    [Silver IV] Title: 30, Time: 968 ms, Memory: 30608 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,21 +1,27 @@
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        char[] s = sc.nextLine().toCharArray();
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] numbers = br.readLine().toCharArray();
+
         int sum = 0;
-        for (int i=0; i<s.length; i++) {
-            sum += s[i] - '0';
+        for (int i=0; i<numbers.length; i++) {
+            sum += numbers[i] - '0';
         }
-        Arrays.sort(s);
-        if (s[0] == '0' && sum%3 == 0) {
-            for (int i=s.length-1; i>=0; i--) {
-                System.out.print(s[i]);
+        Arrays.sort(numbers);
+        
+        if (numbers[0] == '0' && sum % 3 == 0) {
+            for (int i=numbers.length-1; i>=0; i--) {
+                System.out.print(numbers[i]);
             }
             System.out.println();
         } else {
             System.out.println(-1);
         }
+       
     }
 }
