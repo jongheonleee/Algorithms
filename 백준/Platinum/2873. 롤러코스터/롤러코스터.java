0a45b909587170dd44fc62b1f5302d6aeb9335c9
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-28 16:05:20 +0900

    [Platinum III] Title: 롤러코스터, Time: 788 ms, Memory: 101740 KB -BaekjoonHub

--- 백준/Platinum/2873. 롤러코스터/롤러코스터.java
+++ 백준/Platinum/2873. 롤러코스터/롤러코스터.java
@@ -1,96 +1,88 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static void append(StringBuilder sb, char dir, int cnt) {
+    static void append(StringBuilder sb, char ch, int cnt) {
         for (int i=0; i<cnt; i++) {
-            sb.append(dir);
+            sb.append(ch);
         }
     }
-
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int r = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[r][c];
-        for (int i=0; i<r; i++) {
+        int[][] map = new int[n][m];
+        for (int i=0; i<n; i++) {
             String[] line = br.readLine().split(" ");
-
-            for (int j=0; j<c; j++) {
+            for (int j=0; j<m; j++) {
                 map[i][j] = Integer.parseInt(line[j]);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        // 모든 칸을 방문할 수 있는 경우 1
-        if (r % 2 == 1) {
-            for (int i=0; i<r; i++) {
-                if (i%2 == 0) {
-                    append(sb, 'R', c-1);
-                    if (i != r-1) {
-                        append(sb, 'D', 1);
+        StringBuilder sb1 = new StringBuilder();
+        if (n % 2 == 1) {
+            for (int i=0; i<n; i++) {
+                if (i % 2 == 0) {
+                    append(sb1, 'R', m-1);
+                    if (i != n-1) {
+                        append(sb1, 'D', 1);
                     }
                 }
                 else {
-                    append(sb, 'L', c-1);
-                    append(sb, 'D', 1);
+                    append(sb1, 'L', m-1);
+                    append(sb1, 'D', 1);
                 }
             }
-        }
-        // 모든 칸을 방문할 수 있는 경우 2
-        else if (c % 2 == 1) {
-            for (int j=0; j<c; j++) {
-                if (j%2 == 0) {
-                    append(sb, 'D', r-1);
-                    if (j != c-1) {
-                        append(sb, 'R', 1);
+        } else if (m % 2 == 1) {
+            for (int j=0; j<m; j++) {
+                if (j % 2 == 0) {
+                    append(sb1, 'D', n-1);
+                    if (j != m-1) {
+                        append(sb1, 'R', 1);
                     }
-                } else {
-                    append(sb, 'U', r-1);
-                    append(sb, 'R', 1);
+                }
+                else {
+                    append(sb1, 'U', n-1);
+                    append(sb1, 'R', 1);
                 }
             }
-        }
-        // 모든 칸을 방문하는 것이 불가능할 때
-        else {
-            int x, y;
-            x = 0; y = 1;
+        } else {
+            int x=0, y=1;
 
-            for (int i=0; i<r; i++) {
-                for (int j=0; j<c; j++) {
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<m; j++) {
                     if ((i+j) % 2 == 1) {
                         if (map[x][y] > map[i][j]) {
-                            x = i; y = j;
+                            x = i;
+                            y = j;
                         }
                     }
                 }
             }
 
-            int x1 = 0, y1 = 0;
-            int x2 = r-1, y2 = c-1;
+            int x1=0, y1=0;
+            int x2=n-1, y2=m-1;
 
             StringBuilder sb2 = new StringBuilder();
 
             while (x2 - x1 > 1) {
                 if (x1/2 < x/2) {
-                    append(sb, 'R', c-1);
-                    append(sb, 'D', 1);
-                    append(sb, 'L', c-1);
-                    append(sb, 'D', 1);
+                    append(sb1, 'R', m-1);
+                    append(sb1, 'D', 1);
+                    append(sb1, 'L', m-1);
+                    append(sb1, 'D', 1);
 
                     x1 += 2;
                 }
 
-                if (x/2 < x2/2) {
-                    append(sb2, 'R', c-1);
+                if (x2/2 > x/2) {
+                    append(sb2, 'R', m-1);
                     append(sb2, 'D', 1);
-                    append(sb2, 'L', c-1);
+                    append(sb2, 'L', m-1);
                     append(sb2, 'D', 1);
 
                     x2 -= 2;
@@ -99,15 +91,15 @@ public class Main {
 
             while (y2 - y1 > 1) {
                 if (y1/2 < y/2) {
-                    append(sb, 'D', 1);
-                    append(sb, 'R', 1);
-                    append(sb, 'U', 1);
-                    append(sb, 'R', 1);
+                    append(sb1, 'D', 1);
+                    append(sb1, 'R', 1);
+                    append(sb1, 'U', 1);
+                    append(sb1, 'R', 1);
 
                     y1 += 2;
                 }
 
-                if (y/2 < y2/2) {
+                if (y2/2 > y/2) {
                     append(sb2, 'D', 1);
                     append(sb2, 'R', 1);
                     append(sb2, 'U', 1);
@@ -118,17 +110,16 @@ public class Main {
             }
 
             if (y == y1) {
-                append(sb, 'R', 1);
-                append(sb, 'D', 1);
+                append(sb1, 'R', 1);
+                append(sb1, 'D', 1);
             } else {
-                append(sb, 'D', 1);
-                append(sb, 'R', 1);
+                append(sb1, 'D', 1);
+                append(sb1, 'R', 1);
             }
-
             sb2.reverse();
-            sb.append(sb2);
+            sb1.append(sb2);
         }
 
-        System.out.println(sb);
+        System.out.println(sb1);
     }
 }
