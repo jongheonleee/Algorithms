e38fd8b354a9b683f626ec2177edc6d448a17dd8
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-06-30 15:32:30 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 444 ms, Memory: 36700 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,18 +1,24 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void solve(int n, int x, int y, StringBuilder sb) {
+
+    private static void solve(int n, int x, int y, StringBuilder sb) {
         if (n == 0) return;
+
         solve(n-1, x, 6-x-y, sb);
-        sb.append(x + " " + y + "\n");
-        solve(n-1, 6-x-y,y, sb);
+        sb.append(x).append(" ").append(y).append("\n");
+        solve(n-1, 6-x-y, y, sb);
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
         System.out.println((1<<n)-1);
         StringBuilder sb = new StringBuilder();
         solve(n, 1, 3, sb);
         System.out.println(sb);
+
     }
 }
