5a700f07e862b964d6e8b3def1deb7a4f713c8f2
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-01 16:24:00 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,32 +1,47 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
+    
     static int power2(int k) {
         return (1 << k);
     }
-    static int go(int n, int x, int y){
-        if (n==1) {
+    
+    static int solve(int n, int x, int y) {
+        if (n == 1) {
             return 2*x+y;
-        } else {
+        }
+        else {
             if (x < power2(n-1)) {
                 if (y < power2(n-1)) {
-                    return go(n-1,x,y);
-                } else {
-                    return go(n-1,x,y-power2(n-1))+power2(2*n-2);
+                    return solve(n-1, x, y);
                 }
-            } else {
+                else {
+                    return solve(n-1, x, y-power2(n-1)) + power2(2*(n-1));
+                }
+            }
+            
+            else {
                 if (y < power2(n-1)) {
-                    return go(n-1,x-power2(n-1),y)+power2(2*n-2)*2;
-                } else {
-                    return go(n-1,x-power2(n-1),y-power2(n-1))+power2(2*n-2)*3;
+                    return solve(n-1, x-power2(n-1), y) + power2(2*(n-1)) * 2;
+                }
+                else {
+                    return solve(n-1, x-power2(n-1), y-power2(n-1)) + power2(2*(n-1)) * 3;
                 }
             }
         }
+        
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        System.out.println(go(n,x,y));
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        
+        int n = Integer.parseInt(line[0]);
+        int r = Integer.parseInt(line[1]);
+        int c = Integer.parseInt(line[2]);
+
+        System.out.println(solve(n, r, c));
+
+
     }
 }
