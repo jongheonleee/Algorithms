9bd12fac1bd70caa753f9369cca772c56501b5db
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-02 16:58:36 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 332 ms, Memory: 29868 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,42 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    final static char STAR = '*';
-    final static char BLANK = ' ';
-    static void go(char[][] a, int x, int y, int n, char color) {
+    static final char STAR = '*';
+    static final char BLANK = ' ';
+
+    static void go(char[][] a, int x, int y, int size, char color) {
         if (color == BLANK) {
-            for (int i=x; i<x+n; i++) {
-                for (int j=y; j<y+n; j++) {
+            for (int i=x; i<x+size; i++) {
+                for (int j=y; j<y+size; j++) {
                     a[i][j] = BLANK;
                 }
             }
-        } else {
-            if (n == 1) {
-                a[x][y] = STAR;
-            } else {
+        }
+
+        else {
+            if (size == 1) {
+                a[x][y] = color;
+            }
+
+            else {
+                int m = size/3;
+
                 char newColor = STAR;
-                int m = n/3;
                 for (int i=0; i<3; i++) {
                     for (int j=0; j<3; j++) {
-                        if (i==1 && j==1) {
+                        if (i == 1 && j == 1) {
                             newColor = BLANK;
-                        } else {
+                        }
+                        else {
                             newColor = STAR;
                         }
-                        go(a,x+m*i,y+m*j,m,newColor);
+                        go(a, x+m*i, y+m*j, m, newColor);
                     }
                 }
             }
         }
-
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
         char[][] a = new char[n][n];
+
         go(a, 0, 0, n, STAR);
         for (int i=0; i<n; i++) {
             System.out.println(String.valueOf(a[i]));
         }
+
     }
 }
