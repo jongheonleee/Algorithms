5c7a00ed4594379ebedebb4c3a5c3923c2a353ef
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-05 16:08:38 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1504 ms, Memory: 141768 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,85 +1,79 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
+
+
+// O(N * logN) 이하
 public class Main {
 
 
-    // target 보다 크거나 같은 수 중에서 첫번째 위치를 반환
-    public static int lower_bound(int[] cards, int target) {
-        int left = 0, right = cards.length-1, pos = 0;
+    private static int lower_bound(int[] a, int t) {
+        int left = 0, right = a.length-1, pos = 0;
 
         while (left <= right) {
-            int mid = (left + right) /2;
+            int mid = (left+right)/2;
 
-            if (cards[mid] == target) {
+            if (a[mid] == t) {
                 pos = mid;
                 right = mid - 1;
-            }
-
-            else if (cards[mid] > target) {
+            } else if (a[mid] < t) {
+                left = mid + 1;
+            } else {
                 right = mid - 1;
             }
-            else if (cards[mid] < target) {
-                left = mid + 1;
-            }
-
         }
+
         return pos;
-
     }
-
-    // target 보다 큰 수 중에서 첫번째 위치를 반환
-    public static int upper_bound(int[] cards, int target) {
-        int left = 0, right = cards.length-1, pos = 0;
+    
+    private static int upper_bound(int[] a, int t) {
+        int left = 0, right = a.length-1, pos = 0;
 
         while (left <= right) {
-            int mid = (left + right) /2;
+            int mid = (left+right)/2;
 
-            if (cards[mid] == target) {
+            if (a[mid] == t) {
                 left = mid + 1;
                 pos = left;
-            }
-
-            else if (cards[mid] > target) {
+            } else if (a[mid] < t) {
+                left = mid + 1;
+            } else {
                 right = mid - 1;
             }
-            else if (cards[mid] < target) {
-                left = mid + 1;
-            }
-
         }
-        return pos;
 
+        return pos;
     }
 
-    public static void main(String args[]) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        
         int n = Integer.parseInt(br.readLine());
-        int[] cards = new int[n];
-
+        int[] a = new int[n];
         String[] line1 = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            cards[i] = Integer.parseInt(line1[i]);
+            a[i] = Integer.parseInt(line1[i]);
         }
-
+        
         int m = Integer.parseInt(br.readLine());
-        int[] targets = new int[m];
-
+        int[] b = new int[m];
         String[] line2 = br.readLine().split(" ");
         for (int i=0; i<m; i++) {
-            targets[i] = Integer.parseInt(line2[i]);
+            b[i] = Integer.parseInt(line2[i]);
         }
 
-        Arrays.sort(cards);
+        Arrays.sort(a);
+        StringBuilder sb = new StringBuilder();
+
 
         for (int i=0; i<m; i++) {
-            int idx1 = lower_bound(cards, targets[i]);
-            int idx2 = upper_bound(cards, targets[i]);
+            int p1 = lower_bound(a, b[i]);
+            int p2 = upper_bound(a, b[i]);
 
-            sb.append(idx2-idx1).append(" ");
+            sb.append(p2-p1).append(" ");
         }
 
         System.out.println(sb);
+
     }
 }
