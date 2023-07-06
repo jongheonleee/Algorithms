ee2567f3548be3fe224ba134cc056363865b7df2
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-06 16:04:37 +0900

    [Silver II] Title: 종이의 개수, Time: 948 ms, Memory: 319052 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,65 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
 
-    static int a = 0; // -1
-    static int b = 0; // 0
-    static int c = 0; // 1
+    static int[] count = new int[3]; // 0 -> -1, 1 -> 0, 2 -> 1
 
     static int[][] paper;
-    // divide paper
-    public static void go(int x1, int y1, int x2, int y2) {
-        if (!checkRange(x1, y1) || !checkRange(x2, y2)) return;
 
-        if (check(x1, y1, x2, y2)) {
-            if (paper[x1][y1] == -1) {
-                a++;
-            } else if (paper[x1][y1] == 0) {
-                b++;
-            } else {
-                // paper[x1][y1] == 1
-                c++;
-            }
-        }
-        else {
-            // divide
-            int n = (x2-x1+1)/3;
-            int m = (y2-y1+1)/3;
 
-            for (int i=x1; i<x1+3*n; i+=n) {
-                for (int j=y1; j<y1+3*m; j+=m) {
-                    go(i, j, i+n-1, j+m-1);
-                }
-            }
-        }
-    }
-
-    public static boolean check(int x1, int y1, int x2, int y2) {
-        int mark = paper[x1][y1];
+    static boolean isSameNumber(int x1, int y1, int x2, int y2) {
+        int number = paper[x1][y1];
 
         for (int i=x1; i<=x2; i++) {
-            for (int j=y1; j<=y2; j++)
-                if (mark != paper[i][j]) return false;
+            for (int j=y1; j<=y2; j++) {
+                if (number != paper[i][j]) return false;
+            }
         }
 
         return true;
     }
 
-    public static boolean checkRange(int x, int y) {
-        if ((0 <= x && x <= paper.length-1 && 0 <= y && y <= paper[0].length)){
-            return true;
-        }
-        else {
-            return true;
+    static void divide(int x, int y, int len) {
+        if (len == 1 || isSameNumber(x, y, x+len-1, y+len-1)) {
+            count[paper[x][y] + 1] += 1;
+            return;
+        } else {
+            int m = len/3;
+
+            // divide
+            for (int k=0; k<3; k++) {
+                divide(x+m*k, y, m);
+                divide(x+m*k, y+m, m);
+                divide(x+m*k, y+2*m, m);
+            }
         }
     }
-
-
-    public static void main(String args[]) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         paper = new int[n][n];
 
         for (int i=0; i<n; i++) {
@@ -69,9 +48,10 @@ public class Main {
             }
         }
 
-        go(0, 0, n-1, n-1);
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        divide(0, 0, n);
+
+        for (int i=0; i<3; i++) {
+            System.out.println(count[i]);
+        }
     }
 }
