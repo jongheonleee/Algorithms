d0b8e702e20f34cd84fb2b8fc929d87a9b30c90d
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-07 11:23:57 +0900

    [Gold I] Title: 트리의 순회, Time: 448 ms, Memory: 55320 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -1,45 +1,48 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
     static final int LIMIT = 100_000;
     static int[] inOrder = new int[LIMIT+1];
     static int[] postOrder = new int[LIMIT+1];
-
     static int[] positions = new int[LIMIT+1];
+    static StringBuilder sb = new StringBuilder();
 
-    static void solve(int inStart, int inEnd, int postStart, int postEnd) {
+    private static void solve(int inStart, int inEnd, int postStart, int postEnd) {
         if (inStart > inEnd || postStart > postEnd) return;
 
-        // extract root in post order
+        // N
         int root = postOrder[postEnd];
-        System.out.print(root + " ");
+        sb.append(root).append(" ");
 
-        // divide into two trees
         int rootPos = positions[root];
         int left = rootPos - inStart;
 
-        solve(inStart, rootPos-1, postStart, postStart + left - 1);
-        solve(rootPos+1, inEnd, postStart + left, postEnd-1);
+        // L
+        solve(inStart, inStart+left-1, postStart, postStart+left-1);
+
+        // R
+        solve(inStart+left+1, inEnd, postStart+left, postEnd-1);
+
     }
 
-    public static void main(String args[]) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        String[] lineInOrder = br.readLine().split(" ");
+        String[] line1 = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            inOrder[i] = Integer.parseInt(lineInOrder[i]);
-        }
-
-        String[] linePostOrder = br.readLine().split(" ");
-        for (int i=0; i<n; i++) {
-            postOrder[i] = Integer.parseInt(linePostOrder[i]);
-        }
-
-        for (int i=0; i<n; i++) {
+            inOrder[i] = Integer.parseInt(line1[i]);
             positions[inOrder[i]] = i;
         }
+
+        String[] line2 = br.readLine().split(" ");
+        for (int i=0; i<n; i++) {
+            postOrder[i] = Integer.parseInt(line2[i]);
+        }
+
         solve(0, n-1, 0, n-1);
+        System.out.println(sb);
     }
 }
