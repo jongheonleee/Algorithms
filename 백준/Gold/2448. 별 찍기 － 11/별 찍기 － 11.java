39ece8c9cf348a1225626bcc4567f70c969eec0a
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-08 21:07:51 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 488 ms, Memory: 71916 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -1,64 +1,63 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
 
     static final char STAR = '*';
     static final char BLANK = ' ';
 
-    static void go(char[][] a, int x, int y, int n, char color) {
+    static char[][] paper;
+
+    static void divide(int x, int y, int n, char color) {
         if (color == BLANK) {
             int m = 2*n-1;
+
             for (int i=x; i<x+n; i++) {
                 for (int j=0; j<m; j++) {
-                    a[i][j+y+i-x] = BLANK;
+                    paper[i][j+i-x+y] = BLANK;
                 }
                 m -= 2;
             }
-        }
-        else {
-
+        } else if (color == STAR) {
             if (n != 1) {
                 int m = n/2;
 
-                go(a, x, y, m, color);
-                go(a, x+m, y-m, m, color);
-                go(a, x+m, y+m, m, color);
+                divide(x, y, m, STAR);
+                divide(x+m, y-m, m, STAR);
+                divide(x+m, y+m, m, STAR);
 
                 if (n == 3) {
-                    go(a, x+1, y, 1, BLANK);
+                    divide(x+1, y, 1, BLANK);
                 }
                 else {
-                    go(a, x+m, y-m+1, m, BLANK);
+                    divide(x+m, y-m+1, m, BLANK);
                 }
             }
-
         }
-
-
     }
-    public static void main(String args[]) throws IOException {
+
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n =Integer.parseInt(br.readLine());
 
-        char[][] a = new char[n][2*n];
-
+        paper = new char[n][2*n];
         for (int i=0; i<n; i++) {
-            Arrays.fill(a[i], STAR);
+            Arrays.fill(paper[i], STAR);
         }
 
-        go(a, 0, n-1, n, STAR);
+        divide(0, n-1, n, STAR);
         for (int i=0; i<n; i++) {
             for (int j=0; j<n-i-1; j++) {
-                a[i][j] = BLANK;
-                a[i][2*n-j-2] = BLANK;
+                paper[i][j] = BLANK;
+                paper[i][2*n-j-2] = BLANK;
             }
-            a[i][2*n-1] = BLANK;
+            paper[i][2*n-1] = BLANK;
         }
-
 
         for (int i=0; i<n; i++) {
-            System.out.println(String.valueOf(a[i]));
+            System.out.println(String.valueOf(paper[i]));
         }
+
     }
 }
