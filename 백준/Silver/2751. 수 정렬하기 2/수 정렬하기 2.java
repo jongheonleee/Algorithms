880e8e53cace885a7fd839070a79712c2214a01c
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-10 16:17:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1144 ms, Memory: 224444 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -3,15 +3,34 @@ import java.io.*;
 
 public class Main {
 
-    static void shuffle(int[] a){
-        Random random = new Random();
 
-        for (int i=a.length-1; i>0; i--) {
-            int idx = random.nextInt(i+1);
-            int tmp = a[idx];
-            a[idx] = a[i];
-            a[i] = tmp;
+    static int[] merge(int[] left, int[] right) {
+        int[] ans = new int[left.length+ right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) ans[k++] = left[i++];
+            else ans[k++] = right[j++];
         }
+
+        while (i < left.length) ans[k++] = left[i++];
+        while (j < right.length) ans[k++] = right[j++];
+
+        return ans;
+    }
+
+    static int[] merge_sort(int[] a, int start, int end){
+        if (start == end) {
+            int[] tmp = new int[1];
+            tmp[0] = a[start];
+            return tmp;
+        }
+
+        int mid = (start+end)/2;
+        int[] left = merge_sort(a, start, mid);
+        int[] right = merge_sort(a, mid+1, end);
+
+        return merge(left, right);
     }
 
 
@@ -21,10 +40,9 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
         for (int i=0; i<n; i++) a[i] = Integer.parseInt(br.readLine());
-        shuffle(a);
-        Arrays.sort(a);
+        int[] ans = merge_sort(a, 0, n-1);
         for (int i=0; i<n; i++) {
-            bw.write(a[i] + "\n");
+            bw.write(ans[i] + "\n");
         }
         bw.flush();
     }
