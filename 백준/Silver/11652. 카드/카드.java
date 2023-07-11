579f727bf175521f7925645633466599e27a7491
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-11 18:12:39 +0900

    [Silver IV] Title: 카드, Time: 304 ms, Memory: 25212 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,28 +1,37 @@
 import java.util.*;
 import java.io.*;
+
+
 public class Main {
+
+
     public static void main(String args[]) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         long[] a = new long[n];
+
         for (int i=0; i<n; i++) {
-            a[i] = Long.valueOf(bf.readLine());
+            a[i] = Long.parseLong(br.readLine());
         }
+
         Arrays.sort(a);
         long ans = a[0];
         int ans_cnt = 1;
         int cnt = 1;
+
         for (int i=1; i<n; i++) {
             if (a[i] == a[i-1]) {
-                cnt += 1;
+                cnt++;
             } else {
                 cnt=1;
             }
+
             if (ans_cnt < cnt) {
                 ans_cnt = cnt;
                 ans = a[i];
             }
-        }
+         }
         System.out.println(ans);
+
     }
 }
