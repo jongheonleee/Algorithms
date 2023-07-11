84cb35440da7f414f79f0cec12ac8fe8d03e91b7
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-11 20:49:55 +0900

    [Gold II] Title: 버블 소트, Time: 1596 ms, Memory: 78672 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -1,19 +1,26 @@
 import java.util.*;
 import java.io.*;
+
 class Pair implements Comparable<Pair> {
-    int first;
-    int second;
+    int first, second;
+
     Pair(int first, int second) {
         this.first = first;
         this.second = second;
     }
+
+    @Override
     public int compareTo(Pair that) {
         if (this.first < that.first) {
             return -1;
         } else if (this.first == that.first) {
-            if (this.second < that.second) return -1;
-            else if (this.second == that.second) return 0;
-            else return 1;
+            if (this.second < that.second) {
+                return -1;
+            } else if (this.second == that.second) {
+                return 0;
+            } else {
+                return 1;
+            }
         } else {
             return 1;
         }
@@ -22,12 +29,13 @@ class Pair implements Comparable<Pair> {
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         Pair[] a = new Pair[n];
         for (int i=0; i<n; i++) {
-            a[i] = new Pair(Integer.parseInt(bf.readLine()), i);
+            a[i] = new Pair(Integer.parseInt(br.readLine()), i);
         }
+
         Arrays.sort(a);
         int ans = 0;
         for (int i=0; i<n; i++) {
@@ -35,6 +43,8 @@ public class Main {
                 ans = a[i].second - i;
             }
         }
+
         System.out.println(ans+1);
+
     }
 }
