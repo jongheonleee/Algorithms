c2643ef3f2327a23196e79eca8ec56974b3e5466
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-12 17:10:13 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 160 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.java
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.java
@@ -1,31 +1,44 @@
 import java.util.*;
+import java.io.*;
+
+
+
 
 public class Main {
+
     static long calc(int n) {
         long ans = 0;
+
         for (int start=1, len=1; start<=n; start*=10, len++) {
             int end = start*10-1;
             if (end > n) {
                 end = n;
             }
-            ans += (long)(end - start + 1) * len;
+
+            ans += (long)(end-start+1)*len;
         }
+
         return ans;
+
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long k = sc.nextLong();
-        if (calc(n) < k) {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        long k = Long.parseLong(line[1]);
+
+        if (k > calc(n)) {
             System.out.println(-1);
             System.exit(0);
         }
-        int left = 1;
-        int right = n;
-        int ans = 0;
+
+        int left = 1, right = n, ans = 0;
+
         while (left <= right) {
             int mid = (left+right)/2;
             long len = calc(mid);
+
             if (len < k) {
                 left = mid+1;
             } else {
@@ -33,8 +46,11 @@ public class Main {
                 right = mid-1;
             }
         }
+
         String s = Integer.toString(ans);
         long l = calc(ans);
-        System.out.println(s.charAt(s.length()-(int)(l-k)-1));
+
+        System.out.println(s.charAt(s.length()-(int)(l-k+1)));
+
     }
 }
