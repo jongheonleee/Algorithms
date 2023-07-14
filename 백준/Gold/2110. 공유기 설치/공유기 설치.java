7192135dcc624f6048676d9390588410045ebeb5
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-14 12:29:08 +0900

    [Gold IV] Title: 공유기 설치, Time: 304 ms, Memory: 29240 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -3,15 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Main {
 
-
-    static boolean possible(int[] a, int k, int c) {
+    static boolean check(int[] a, int len, int c) {
         int cnt = 1;
         int last = a[0];
 
         for (int h : a) {
-            if (h - last >= k) {
+            int diff = h - last;
+            if (diff >= len) {
                 cnt++;
                 last = h;
             }
@@ -21,30 +22,32 @@ public class Main {
     }
 
 
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
 
         int[] a = new int[n];
-        for (int i=0; i<n; i++) a[i] = Integer.parseInt(br.readLine());
+        for (int i=0; i<n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
         Arrays.sort(a);
-
-        int left = 1, right = a[n-1]-a[0], ans = 0;
+        int max = a[a.length-1] - a[0];
+        int left = 1, right = max, ans = 0;
 
         while (left <= right) {
             int mid = (left+right)/2;
 
-            if (possible(a, mid, c)) {
+            if (check(a, mid, c)) {
                 ans = mid;
                 left = mid+1;
             } else {
                 right = mid-1;
             }
         }
-        System.out.println(ans);
 
+        System.out.println(ans);
 
     }
 }
