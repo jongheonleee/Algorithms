e6188cc17996628ee895a45504b5560f8d55714e
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-17 20:22:48 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 152 ms, Memory: 15016 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -1,51 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.util.*;
+
+
+
+
+
 public class Main {
-    public static int go(int[] a, int mid) {
-        int n = a.length;
+
+    static boolean check(int[] a, int mid, int k) {
+        int cnt = 1;
         int t1 = a[0];
         int t2 = a[0];
-        int ans = 1;
-        for (int i=1; i<n; i++) {
-            if (t1 > a[i]) {
-                t1 = a[i];
-            }
-            if (t2 < a[i]) {
-                t2 = a[i];
-            }
-            if (t2-t1 > mid) {
-                ans += 1;
-                t1 = a[i];
-                t2 = a[i];
-            }
-        }
-        return ans;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] a = new int[n];
-        int l = 0; // 최소값 0으로 설정
-        int r = 0;
 
-        for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
-            if (r < a[i]) {
-                r = a[i]; // a의 최대값 저장
+        for (int i=1; i<a.length; i++) {
+            if (t1 > a[i]) t1 = a[i];
+            if (t2 < a[i]) t2 = a[i];
+            if (t2-t1 > mid) {
+                cnt++;
+                t1 = a[i];
+                t2 = a[i];
             }
         }
-				
-        int ans = r;
-        while (l <= r) {
-            int mid = (l+r)/2;
-            if (go(a, mid) <= k) {
-                r = mid-1;
-								// 최소값 저장
-                if (ans > mid) {
-                    ans = mid;
-                }
+
+        return cnt <= k;
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[n];
+        String[] line = br.readLine().split(" ");
+        int left = 0, right = 0;
+        for (int i=0; i<n; i++) {
+            a[i] = Integer.parseInt(line[i]);
+            right = a[i] > right ? a[i] : right;
+        }
+
+        int ans = right;
+        while (left <= right) {
+            int mid = (left+right)/2;
+
+            if (check(a, mid, k)) {
+                right = mid-1;
+                ans = ans > mid ? mid : ans;
             } else {
-                l = mid+1;
+                left = mid+1;
             }
         }
         System.out.println(ans);
