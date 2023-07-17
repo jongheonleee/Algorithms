471d2ea9607e0fe910b282e76701644096283d0c
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-17 15:41:29 +0900

    [Silver I] Title: 기타 레슨, Time: 320 ms, Memory: 25376 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -10,28 +10,18 @@ import java.util.*;
 
 public class Main {
 
-    static boolean isPossible(int[] a, int k) {
-        for (int i=0; i<a.length; i++) {
-            if (a[i] > k) return false;
-        }
-
-        return true;
-    }
-
-
+    static final int INIT = -1;
     static boolean check(int[] a, int k, int m) {
-        if (isPossible(a, k) == false) return false;
-
         int acc = 0, cnt = 1;
 
         for (int i=0; i<a.length; i++) {
-            if (acc + a[i] <= k) {
-                acc += a[i];
-            } else {
-                cnt += 1;
+            if (acc + a[i] > k) {
+                cnt+= 1;
                 acc = a[i];
             }
-
+            else {
+                acc += a[i];
+            }
         }
 
         return cnt <= m;
@@ -42,31 +32,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
-
         int[] a = new int[n];
         String[] line = br.readLine().split(" ");
-        int sum = 0, min = -1;
+        int left = INIT, right = 0, ans = 0;
         for (int i=0; i<n; i++) {
             a[i] = Integer.parseInt(line[i]);
-            sum += a[i];
-            if (min == -1 || min != -1 && min > a[i]) min = a[i];
+            if (left == INIT || left != INIT && left < a[i]) left = a[i];
+            right += a[i];
         }
-
-
-        int left = min, right = sum, ans = 0;
 
         while (left <= right) {
             int mid = (left+right)/2;
 
             if (check(a, mid, m)) {
                 ans = mid;
-                right = mid - 1;
+                right = mid-1;
             } else {
-                left = mid + 1;
+                left = mid+1;
             }
         }
-
         System.out.println(ans);
-
     }
 }
