1420bf7c6e2d9e750c0a2741c68e913ad29c46b7
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-07-24 12:04:47 +0900

    [Bronze II] Title: 양념 반 후라이드 반, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/16917. 양념 반 후라이드 반/양념 반 후라이드 반.java
+++ 백준/Bronze/16917. 양념 반 후라이드 반/양념 반 후라이드 반.java
@@ -2,45 +2,51 @@ import java.io.*;
 import java.util.*;
 
 
+
 public class Main {
 
+
+
     public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int ans = 0;
-
-        if (x > 0 && y > 0) {
-            int min = Math.min(x, y);
-            if (2 * c >= a + b) {
-                ans += a * min + b * min;
-            } else {
-                ans = 2 * c * min;
-            }
-            x -= min; y -= min;
-        }
-
-        if (x > 0) {
-            if (2*c >= a) {
-                ans += a * x;
-            } else {
-                ans += 2 * c * x;
-            }
-        }
-
-        if (y > 0) {
-            if (2*c >= b) {
-                ans += b * y;
-            } else {
-                ans += 2 * c * y;
-            }
-        }
-
-        System.out.println(ans);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int a = Integer.parseInt(line[0]);
+        int b = Integer.parseInt(line[1]);
+        int c = Integer.parseInt(line[2]);
+        int x = Integer.parseInt(line[3]);
+        int y = Integer.parseInt(line[4]);
         
+        
+        int price = 0;
+        while (x>0 && y>0) {
+            if (a+b >= 2*c) {
+                price += 2*c;
+            } else {
+                price += a+b;
+            }
+            x--;
+            y--;
+        }
+        
+        while (x>0) {
+            if (a >= 2*c) {
+                price += 2*c;
+            } else {
+                price += a;
+            }
+            x--;
+        }
+        
+        while (y>0) {
+            if (b >= 2*c) {
+                price += 2*c;
+            } else {
+                price += b;
+            }
+            y--;
+        }
+
+        System.out.println(price);
 
     }
 }
