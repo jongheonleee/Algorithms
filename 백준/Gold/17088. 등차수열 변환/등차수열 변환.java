10a6d5a419847bb824a86df101ae04341a4463d1
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-08-08 17:33:44 +0900

    [Gold V] Title: 등차수열 변환, Time: 336 ms, Memory: 27652 KB -BaekjoonHub

--- 백준/Gold/17088. 등차수열 변환/등차수열 변환.java
+++ 백준/Gold/17088. 등차수열 변환/등차수열 변환.java
@@ -1,45 +1,65 @@
 import java.util.*;
+import java.io.*;
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
         int[] a = new int[n];
+        String[] line = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(line[i]);
         }
+
+        int ans = -1;
         if (n == 1) {
             System.out.println(0);
             System.exit(0);
         }
-        int ans = -1;
         for (int d1=-1; d1<=1; d1++) {
             for (int d2=-1; d2<=1; d2++) {
-                int change = 0;
-                if (d1 != 0) change += 1;
-                if (d2 != 0) change += 1;
                 int a0 = a[0]+d1;
-                int diff = (a[1]+d2) - a0;
+                int a1 = a[1]+d2;
+
+                int cnt = 0;
+                if (d1 != 0) cnt++;
+                if (d2 != 0) cnt++;
+
                 boolean ok = true;
-                int an = a0 + diff;
+                int d = a1-a0;
+
+                a0 = a1;
                 for (int i=2; i<n; i++) {
-                    an += diff;
-                    if (a[i] == an) continue;
-                    if (a[i]-1 == an) {
-                        change += 1;
-                    } else if (a[i]+1 == an) {
-                        change += 1;
+                    a1 = a[i];
+
+                    int diff = a1-a0;
+                    if (diff == d) {
+                        a0 = a1;
+                        continue;
+                    } else if (diff+1 == d) {
+                        cnt++;
+                        a1++;
+                        a0 = a1;
+                    } else if (diff-1 == d) {
+                        cnt++;
+                        a1--;
+                        a0 = a1;
                     } else {
                         ok = false;
                         break;
                     }
                 }
+
                 if (ok) {
-                    if (ans == -1 || ans > change) {
-                        ans = change;
-                    }
+                    if (ans == -1 || ans > cnt) ans = cnt;
                 }
             }
         }
         System.out.println(ans);
+
     }
 }
