70f9e89c994cf0c35971fb437a3b3bbd9acd827b
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-08-14 15:00:22 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 136 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,56 +1,86 @@
 import java.util.*;
+import java.io.*;
+
+
+
 public class Main {
+
+    static int n;
     static int[][] a;
     static long[][][] d;
-    static int n;
+
+
     static long go(int x, int y, int dir) {
         if (x == n-1 && y == n-1) {
             return 1;
         }
         long ans = d[x][y][dir];
         if (ans != -1) return ans;
+
         ans = 0;
-        if (dir == 0) { // -
-            if (y+1 < n && a[x][y+1] == 0) {
+        if (dir == 0) {
+            // 가로 -> 가로, 대각선
+            if (0 <= y+1 && y+1 < n && a[x][y+1] == 0) {
                 ans += go(x, y+1, 0);
             }
-            if (x+1 < n && y+1 < n && a[x][y+1] == 0 && a[x+1][y] == 0 && a[x+1][y+1] == 0) {
+
+            if (0 <= x+1 && x+1 < n && 0 <= y+1 && y+1 < n &&
+                    a[x][y+1] == 0 && a[x+1][y] == 0 && a[x+1][y+1] == 0) {
                 ans += go(x+1, y+1, 1);
             }
-        } else if (dir == 1) { // dig
-            if (y+1 < n && a[x][y+1] == 0) {
+        } else if (dir == 1) {
+            // 대각선 -> 가로, 대각선, 세로
+            if (0 <= y+1 && y+1 < n && a[x][y+1] == 0) {
                 ans += go(x, y+1, 0);
             }
-            if (x+1 < n && a[x+1][y] == 0) {
-                ans += go(x+1, y, 2);
-            }
-            if (x+1 < n && y+1 < n && a[x][y+1] == 0 && a[x+1][y] == 0 && a[x+1][y+1] == 0) {
+
+            if (0 <= x+1 && x+1 < n && 0 <= y+1 && y+1 < n &&
+                    a[x][y+1] == 0 && a[x+1][y] == 0 && a[x+1][y+1] == 0) {
                 ans += go(x+1, y+1, 1);
             }
-        } else if (dir == 2) { // |
-            if (x+1 < n && a[x+1][y] == 0) {
+
+            if (0 <= x+1 && x+1 < n && a[x+1][y] == 0) {
                 ans += go(x+1, y, 2);
             }
-            if (x+1 < n && y+1 < n && a[x][y+1] == 0 && a[x+1][y] == 0 && a[x+1][y+1] == 0) {
+
+        } else {
+            // 세로 -> 대각선, 세로
+            if (0 <= x+1 && x+1 < n && 0 <= y+1 && y+1 < n &&
+                    a[x][y+1] == 0 && a[x+1][y] == 0 && a[x+1][y+1] == 0) {
                 ans += go(x+1, y+1, 1);
+            }
+
+            if (0 <= x+1 && x+1 < n && a[x+1][y] == 0) {
+                ans += go(x+1, y, 2);
             }
         }
+
         d[x][y][dir] = ans;
         return ans;
+
+
+
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
         a = new int[n][n];
         d = new long[n][n][3];
         for (int i=0; i<n; i++) {
+            String[] line = br.readLine().split(" ");
             for (int j=0; j<n; j++) {
-                a[i][j] = sc.nextInt();
-                for (int k=0; k<3; k++) {
-                    d[i][j][k] = -1;
+                a[i][j] = Integer.parseInt(line[j]);
+                for (int l=0; l<3; l++) {
+                    d[i][j][l] = -1;
                 }
             }
         }
-        System.out.println(go(0, 1, 0));
+
+        long ans = go(0, 1, 0);
+        System.out.println(ans);
+
     }
 }
