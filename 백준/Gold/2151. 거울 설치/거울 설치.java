0edaea6b0631d6392b28711321103a56be1bed8d
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-08-30 11:04:55 +0900

    [Gold III] Title: 거울 설치, Time: 220 ms, Memory: 24700 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.java
+++ 백준/Gold/2151. 거울 설치/거울 설치.java
@@ -4,16 +4,20 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 
-class Pair {
+/**
+ * 1. 방 구현
+ * 2. 문, 거울을 그래프로 구현
+ * 3. bfs 탐색을 통해 결과 출력
+ */
+
+class Node {
     int x, y;
 
-    Pair (int x, int y) {
+    Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
-
 
 public class Main {
 
@@ -25,10 +29,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        // 1~2. 방 구현 및 그래프 구현
         String[] room = new String[n];
-        int start = -1, end = -1;
-        int[][] number = new int[n][n];
-        ArrayList<Pair> graph = new ArrayList<>();
+        ArrayList<Node> graph = new ArrayList<>();
+        int[][] nodeNumber = new int[n][n];
+        int startNumber = -1, endNumber = -1;
         for (int i=0; i<n; i++) {
             room[i] = br.readLine();
 
@@ -36,61 +41,68 @@ public class Main {
                 char ch = room[i].charAt(j);
 
                 if (ch == '#') {
-                    if (start == -1) {
-                        start = graph.size();
+                    if (startNumber == -1) {
+                        startNumber = graph.size();
                     } else {
-                        end = graph.size();
+                        endNumber = graph.size();
                     }
-
-                    graph.add(new Pair(i, j));
-                    number[i][j] = graph.size()-1;
-                }else if (ch == '!') {
-                    graph.add(new Pair(i, j));
-                    number[i][j] = graph.size()-1;
+                    graph.add(new Node(i, j));
+                    nodeNumber[i][j] = graph.size()-1;
+                } else if (ch == '!') {
+                    graph.add(new Node(i, j));
+                    nodeNumber[i][j] = graph.size()-1;
                 }
             }
         }
 
+
+        // 간선, 노드끼리 연결 해주기
         int m = graph.size();
         boolean[][] edges = new boolean[m][m];
-
-        for (int i=0; i<m; i++) {
-            Pair p = graph.get(i);
+        for (int from=0; from<m; from++) {
+            Node node = graph.get(from);
 
             for (int k=0; k<4; k++) {
-                int x = p.x+dx[k];
-                int y = p.y+dy[k];
+                int x = node.x + dx[k];
+                int y = node.y + dy[k];
 
-                while(0 <= x && x < n && 0 <= y && y < n) {
+                while (0 <= x && x < n && 0 <= y && y < n) {
                     char ch = room[x].charAt(y);
-
-                    if (ch == '*') break;
+                    int to = nodeNumber[x][y];
+                    
                     if (ch == '!' || ch == '#') {
-                        edges[i][number[x][y]] = true;
+                        edges[from][to] = true;
+                    } else if (ch == '*') {
+                        break;
                     }
+                    
                     x += dx[k];
                     y += dy[k];
                 }
             }
         }
 
-        Queue<Integer> q = new LinkedList<>();
+
+        // 3. bfs 탐색
         int[] dist = new int[m];
         Arrays.fill(dist, -1);
-        q.add(start);
-        dist[start] = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(startNumber);
+        dist[startNumber] = 0;
 
         while (!q.isEmpty()) {
-            int now = q.remove();
-            for (int i=0; i<m; i++) {
-                if (edges[now][i] && dist[i] == -1) {
-                    dist[i] = dist[now]+1;
-                    q.add(i);
+            int from = q.remove();
+
+            for (int to=0; to<m; to++) {
+                if (edges[from][to] && dist[to] == -1) {
+                    dist[to] = dist[from]+1;
+                    q.add(to);
                 }
             }
         }
 
-        System.out.println(dist[end]-1);
+        System.out.println(dist[endNumber]-1);
 
 
     }
