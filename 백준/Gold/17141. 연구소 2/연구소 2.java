e00a2b81cf510e0e841da2ce800af5432de394ac
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-08-31 14:21:42 +0900

    [Gold IV] Title: 연구소 2, Time: 504 ms, Memory: 52604 KB -BaekjoonHub

--- 백준/Gold/17141. 연구소 2/연구소 2.java
+++ 백준/Gold/17141. 연구소 2/연구소 2.java
@@ -1,65 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 class Pair {
     int x, y;
 
-    Pair(int x, int y) {
+    Pair (int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
-
 public class Main {
 
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
+    static int row, col, m, ans;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static int[][] laboratory;
+    static ArrayList<Pair> candidates = new ArrayList<>();
+    static boolean[] selected;
 
-    static boolean validate(int bit, int m) {
-        while (bit > 0) {
-            int left = bit%2;
-            if (left == 1) m -= 1;
-            bit = bit >> 1;
+    static void select(int index, int count) {
+        if (index == candidates.size()) {
+            if (count == m) {
+                bfs();
+            }
+            return;
         }
 
-        return m == 0;
+        selected[index] = true;
+        select(index+1, count+1);
+
+        selected[index] = false;
+        select(index+1, count);
     }
 
-    static int calc(int[][] laboratory, int[][] dist) {
-        boolean ok = true;
-        int max = 0;
-
-        for (int i=0; i<laboratory.length; i++) {
-            for (int j=0; j<laboratory[0].length; j++) {
-                if (laboratory[i][j] != 1 && dist[i][j] == -1) ok = false;
-                if (max < dist[i][j]) max = dist[i][j];
-            }
-        }
-
-        return ok == true ? max : -1;
-    }
-
-    static int[][] bfs(ArrayList<Pair> store, int[][] laboratory) {
-        int[][] dist = new int[laboratory.length][laboratory[0].length];
-        for (int i=0; i<dist.length; i++) {
-            for (int j=0; j<dist[0].length; j++) {
-                dist[i][j] = -1;
-            }
+    static void bfs() {
+        int[][] dist = new int[row][col];
+        for (int i=0; i<row; i++) {
+            Arrays.fill(dist[i], -1);
         }
 
         Queue<Pair> q = new LinkedList<>();
-        for (Pair selected : store) {
-            int x = selected.x;
-            int y = selected.y;
-            q.add(new Pair(x, y));
-            dist[x][y] = 0;
+        for (int i=0; i<candidates.size(); i++) {
+            if (selected[i] == true) {
+                Pair p = candidates.get(i);
+                int x = p.x;
+                int y = p.y;
+
+                dist[x][y] = 0;
+                q.add(new Pair(x, y));
+            }
         }
 
         while (!q.isEmpty()) {
             Pair p = q.remove();
+
             int x = p.x;
             int y = p.y;
 
@@ -67,58 +60,45 @@ public class Main {
                 int nx = x+dx[k];
                 int ny = y+dy[k];
 
-                if (0 <= nx && nx < laboratory.length && 0 <= ny && ny < laboratory[0].length) {
-                    if (laboratory[nx][ny] != 1 && dist[nx][ny] == -1) {
-                        q.add(new Pair(nx, ny));
-                        dist[nx][ny] = dist[x][y] + 1;
-                    }
-                }
+                if (!(0 <= nx && nx < row && 0 <= ny && ny < col)) continue;
+                if (laboratory[nx][ny] == 1 || dist[nx][ny] != -1) continue;
+
+                dist[nx][ny] = dist[x][y]+1;
+                q.add(new Pair(nx, ny));
             }
         }
 
-        return dist;
+        int res = 0;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (laboratory[i][j] != 1 && dist[i][j] == -1) return;
+                if (res < dist[i][j]) res = dist[i][j];
+            }
+        }
+
+        if (ans == -1 || ans > res) ans = res;
     }
 
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        row = col = sc.nextInt();
+        m = sc.nextInt();
+        ans = -1;
+        laboratory = new int[row][col];
 
-        ArrayList<Pair> candidates = new ArrayList<>();
-        int[][] laboratory = new int[n][n];
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
                 laboratory[i][j] = sc.nextInt();
+
                 if (laboratory[i][j] == 2) {
                     candidates.add(new Pair(i, j));
                 }
             }
         }
-
-        int ans = -1;
-        for (int i=0; i<(1<<candidates.size()); i++) {
-            if (validate(i, m)) {
-                ArrayList<Pair> store = new ArrayList<>();
-                for (int j=0; j<candidates.size(); j++) {
-                    if ((i&(1<<j)) != 0) {
-                        store.add(candidates.get(j));
-                    }
-                }
-                
-
-                int[][] dist = bfs(store, laboratory);
-                int tmp = calc(laboratory, dist);
-                if (tmp != -1 && (ans == -1 || ans > tmp)) {
-                    ans = tmp;
-                }
-            }
-        }
-
+        selected = new boolean[candidates.size()];
+        select(0, 0);
         System.out.println(ans);
 
-
-
     }
-
 }
