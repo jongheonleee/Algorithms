5ee5b1e043ba990ef9df25a2f1585b3376dfed53
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-09-08 09:28:50 +0900

    [Bronze II] Title: 부분 문자열, Time: 348 ms, Memory: 47176 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.java
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.java
@@ -3,37 +3,33 @@ import java.util.*;
 
 
 
-
-
 public class Main {
-
-    static int base = 256;
-    static int mod = 127;
+    static final int BASE = 256;
+    static final int MOD = 127;
 
     static int hash(String str) {
         int ans = 0;
 
         for (char ch : str.toCharArray()) {
-            ans = (ans * base + ch) % mod;
+            ans = (ans * BASE + ch) % MOD;
         }
 
         return ans;
     }
 
-    static int match(String s, String p) {
+    static int search(String s, String p) {
         int n = s.length();
         int m = p.length();
 
         if (n < m) return 0;
 
         int first = 1;
-        int hashS = hash(s.substring(0, m));
-        int hashP = hash(p);
-
         for (int i=0; i<m-1; i++) {
-            first = (first * base) % mod;
+            first = (first * BASE) % MOD;
         }
 
+        int hashS = hash(s.substring(0, m));
+        int hashP = hash(p);
 
         for (int i=0; i<=n-m; i++) {
             if (hashS == hashP) {
@@ -43,21 +39,19 @@ public class Main {
             }
 
             if (i+m < n) {
-                hashS = hashS-(s.charAt(i)*first)%mod;
-                hashS = (hashS+mod)%mod;
-                hashS = ((hashS*base)%mod + s.charAt(i+m))%mod;
+                hashS = (hashS - first * s.charAt(i)) % MOD;
+                hashS = (hashS + MOD) % MOD;
+                hashS = (hashS * BASE + s.charAt(i+m)) % MOD;
             }
         }
 
         return 0;
     }
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         String p = br.readLine();
 
-        System.out.println(match(s, p));
+        System.out.println(search(s, p));
     }
 }
