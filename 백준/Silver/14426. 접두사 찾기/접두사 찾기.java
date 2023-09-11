3b8f15d6e8cee696ea1b2f4df2bf49a26853e107
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-09-11 10:00:07 +0900

    [Silver I] Title: 접두사 찾기, Time: 500 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
@@ -1,51 +1,73 @@
 import java.io.*;
 import java.util.*;
 
-class Node {
-    char val;
-    HashMap<Character, Node> store;
-
-    Node(char val) {
-        this.val = val;
-        this.store = new HashMap<>();
-    }
-}
 
 class Trie {
 
-    Node root;
+    class Node {
+        int[] children;
+        boolean isWord;
+
+        Node() {
+            children = new int[26];
+            Arrays.fill(children, -1);
+            isWord = false;
+        }
+    }
+
+    ArrayList<Node> trie;
+    int root;
+
+    int init() {
+        Node x = new Node();
+        trie.add(x);
+        return trie.size()-1;
+    }
 
     Trie() {
-        root = new Node('#');
+        trie = new ArrayList<>();
+        root = init();
     }
 
-    public void push(String str) {
-        Node curr = this.root;
-
-        for (char ch : str.toCharArray()) {
-            if (!curr.store.containsKey(ch)) {
-                curr.store.put(ch, new Node(ch));
-            }
-            curr = curr.store.get(ch);
-        }
+    void add(String s) {
+        add(root, s, 0);
     }
 
-    public boolean check(String pattern) {
-        boolean ok = true;
-        Node curr = this.root;
-
-        for (char p : pattern.toCharArray()) {
-            if (curr.store.containsKey(p)) {
-                curr = curr.store.get(p);
-            } else {
-                ok = false;
-                break;
-            }
+    void add(int node, String s, int idx) {
+        if (idx == s.length()) {
+            trie.get(node).isWord = true;
+            return;
         }
 
-        return ok;
+        int ch = s.charAt(idx) - 'a';
+        if (trie.get(node).children[ch] == -1) {
+            int next = init();
+            trie.get(node).children[ch] = next;
+        }
+
+        int child = trie.get(node).children[ch];
+        add(child, s, idx+1);
+    }
+
+    boolean search(String s) {
+        return search(root, s, 0);
+    }
+
+    boolean search(int node, String s, int idx) {
+        if (node == -1) {
+            return false;
+        }
+
+        if (idx == s.length()) {
+            return true;
+        }
+
+        int ch = s.charAt(idx) - 'a';
+        int next = trie.get(node).children[ch];
+        return search(next, s, idx+1);
     }
 }
+
 
 public class Main {
 
@@ -53,27 +75,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         Trie trie = new Trie();
+
         for (int i=0; i<n; i++) {
-            String str = br.readLine();
-            trie.push(str);
+            String s = br.readLine();
+            trie.add(s);
         }
 
         int ans = 0;
         for (int i=0; i<m; i++) {
-            String pt = br.readLine();
-            boolean ok = trie.check(pt);
-            if (ok) {
-                ans += 1;
-            }
+            String s = br.readLine();
+            boolean ok = trie.search(s);
+            if (ok) ans += 1;
         }
 
-
         System.out.println(ans);
-
     }
 }
