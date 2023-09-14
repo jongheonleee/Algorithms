96ff0f31d5d706008d2df59ffc6e8294821e92f6
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2023-09-14 19:12:09 +0900

    [Silver I] Title: 봄버맨, Time: 364 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.java
+++ 백준/Silver/16918. 봄버맨/봄버맨.java
@@ -12,43 +12,40 @@ class Pair {
 
 
 /**
- * 만약, 폭탄이 폭발했을 때, 인접한 칸에 폭탄이 있는 경우에는 인접한 폭탄은 폭발 없이 파괴된다.
- * 따라서, 연쇄 반응은 없다.
+ * 큐에 기존에 설치된 폭탄을 터트리는 방식
  */
 public class Main {
-    static int r, c, n;
-
+    static int row, col;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    
     static char[][] map;
-    
-    static void simulate() {
+
+    // 봄버맨의 행동 실행
+    static void simulate(int second) {
         Queue<Pair> q = new LinkedList<>();
         // 기존에 설치된 폭탄 큐에 담기
-        for (int x=0; x<r; x++) {
-            for (int y=0; y<c; y++) {
+        for (int x=0; x<row; x++) {
+            for (int y=0; y<col; y++) {
                 if (map[x][y] == 'O') {
                     q.add(new Pair(x, y));
                 }
             }
         }
-        
+
         while (true) {
-            // 1초 후 -> 나머지 부분에 폭탄을 설칠함
+            // 2초 후 -> 나머지 부분에 폭탄을 설치함
             // "다음 1초 동안 폭탄이 설치되어 있지 않은 모든 칸에 폭탄을 설치한다"
-            for (int x=0; x<r; x++) {
-                for (int y=0; y<c; y++) {
+            for (int x=0; x<row; x++) {
+                for (int y=0; y<col; y++) {
                     if (map[x][y] == '.') {
                         map[x][y] = 'O';
                     }
                 }
             }
-            
-            n -= 1;
-            if (n == 0) break;
+            second -= 1;
+            if (second == 0) break;
 
-            // 2초 후 -> 기존에 설치한 폭탄을 터트림
+            // 3초 후 -> 기존에 설치한 폭탄을 터트림
             // "1초가 지난 후에 3초 전에 설치된 폭탄이 모두 폭발한다."
             while (!q.isEmpty()) {
                 Pair p = q.remove();
@@ -61,18 +58,18 @@ public class Main {
                     int nx = x+dx[k];
                     int ny = y+dy[k];
 
-                    if (0 <= nx && nx < r && 0 <= ny && ny < c) {
+                    if (0 <= nx && nx < row && 0 <= ny && ny < col) {
                         map[nx][ny] = '.';
                     }
                 }
             }
-            n -= 1;
-            if (n == 0) break;
+            second -= 1;
+            if (second == 0) break;
 
             // "3과 4를 반복한다."
-            // 따라서, 그 다음 과정을 위해서 폭탄을 담아줌
-            for (int x=0; x<r; x++) {
-                for (int y=0; y<c; y++) {
+            // 그 다음 과정을 위해서 남아있는 폭탄을 큐에 담아줌
+            for (int x=0; x<row; x++) {
+                for (int y=0; y<col; y++) {
                     if (map[x][y] == 'O') {
                         q.add(new Pair(x, y));
                     }
@@ -80,11 +77,12 @@ public class Main {
             }
         }
     }
-    
+
+    // map을 출력해줌
     static void show() {
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i<r; i++) {
-            for (int j=0; j<c; j++) {
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
                 sb.append(map[i][j]);
             }
             sb.append("\n");
@@ -97,29 +95,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
-        r = Integer.parseInt(input[0]);
-        c = Integer.parseInt(input[1]);
-        n = Integer.parseInt(input[2]);
+        row = Integer.parseInt(input[0]);
+        col = Integer.parseInt(input[1]);
+        int second = Integer.parseInt(input[2]);
 
         // 입력값을 통해서 지도 구현
-        map = new char[r][c];
-        for (int i=0; i<r; i++) {
+        map = new char[row][col];
+        for (int i=0; i<row; i++) {
             String line = br.readLine();
-            for (int j=0; j<c; j++) {
+            for (int j=0; j<col; j++) {
                 map[i][j] = line.charAt(j);
             }
         }
-        
+
 
         // "다음 1초 동안 봄버맨은 아무것도 하지 않는다."
-        n -= 1;
-        if (n == 0) {
+        second -= 1;
+        if (second == 0) {
             show();
             System.exit(0);
         }
 
-        simulate();
-        show();
+        simulate(second); // n초 동안 봄버맨 실행시키기
+        show(); // n 초 이후에 map 출력하기
 
     }
 }
