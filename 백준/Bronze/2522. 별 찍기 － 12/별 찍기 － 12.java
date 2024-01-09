fcd834ab249191772e50e6456932286abfdb388d
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2024-01-09 13:37:29 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 136 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -4,6 +4,10 @@ import java.io.*;
 
 public class Main {
 
+    private static final StringBuilder sb = new StringBuilder();
+    private static final String STAR = "*";
+    private static final String BLANK = " ";
+    private static final String ENTER = "\n";
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,13 +18,14 @@ public class Main {
         for (int i=0; i<2*n-1; i++) {
             for (int j=0; j<n; j++) {
                 if ((i < n && i+j >= n-1) || (i>=n && i-j<n)) {
-                    System.out.print("*");
+                    sb.append(STAR);
                 } else {
-                    System.out.print(" ");
+                    sb.append(BLANK);
                 }
             }
-            System.out.println();
+            sb.append(ENTER);
         }
 
+        System.out.println(sb);
     }
 }
