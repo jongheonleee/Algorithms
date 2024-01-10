8cd6b686bcb4d0f7cb18ec6e76a17a8d05cce649
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2024-01-10 17:00:49 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 132 ms, Memory: 17884 KB -BaekjoonHub

--- 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.io.*;
+        import java.io.*;
 
 
 public class Main {
@@ -9,32 +9,33 @@ public class Main {
     private static final String BLANK = " ";
     private static final String ENTER = "\n";
 
+    private static void go(int n, int i, int j) {
+        if (i == n) {
+            System.out.println(sb);
+            return;
+        }
+        int mid = (2*n-1)/2;
+        if (j < 2*n-1) {
+            if (i < n-1) {
+                if (j == mid-i || j == mid+i) {
+                    sb.append(STAR);
+                } else if (j < mid-i || mid-i<j&&j<mid+i) {
+                    sb.append(BLANK);
+                }
+                go(n, i, j+1);
+            } else {
+                sb.append(STAR);
+                go(n, i, j+1);
+            }
+        } else {
+            sb.append(ENTER);
+            go(n, i+1, 0);
+        }
+    }
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int mid = (2*n-1)/2;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<2*n-1; j++) {
-                if (i == 0) {
-                    if (j == mid) {
-                        sb.append(STAR);
-                    } else if (j < mid - i){
-                        sb.append(BLANK);
-                    }
-                } else if (0 < i && i < n-1) {
-                    if (j == mid - i || j == mid + i) {
-                        sb.append(STAR);
-                    } else if (j < mid - i || mid - i < j && j < mid + i){
-                        sb.append(BLANK);
-                    }
-                } else {
-                    // i == n
-                    sb.append(STAR);
-                }
-            }
-            sb.append(ENTER);
-        }
-
-        System.out.println(sb);
+        go(n, 0, 0);
     }
 }
