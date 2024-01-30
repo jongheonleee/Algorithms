84672cf9dce9f40b0926eff7515d9701b101dfeb
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2024-01-30 18:10:33 +0900

    [Gold V] Title: A와 B 2, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,41 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+import java.util.Map.Entry;
+
 
 public class Main {
 
-    static String reverse(String str) {
-        return new StringBuilder(str).reverse().toString();
+    private static String cut(String s) {
+        return s.substring(0, s.length()-1);
     }
-
-    static String cut(String str) {
-        return str.substring(0, str.length()-1);
+    private static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
     }
-
-    static boolean can(String s, String t) {
-        if (s.equals(t)) {
-            return true;
-        }
+    private static boolean check(String s, String t) {
+        if (s.equals(t)) return true;
 
         if (t.length() > 0) {
-            if (t.charAt(t.length()-1) == 'A' && can(s, cut(t))) {
+            if (t.charAt(t.length()-1) == 'A' && check(s, cut(t))) {
                 return true;
             }
-            if (t.charAt(0) == 'B' && can(s, cut(reverse(t)))) {
+
+            if (t.charAt(0) == 'B' && check(s, cut(reverse(t)))) {
                 return true;
             }
         }
-
 
         return false;
     }
-
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         String t = br.readLine();
-
-        int ans = (can(s, t) ? 1 : 0);
-        System.out.println(ans);
-
+        System.out.println(check(s, t) ? 1 : 0);
     }
 }
+
+
