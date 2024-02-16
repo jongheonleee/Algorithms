46dc1a47784a8778b65b2e7cb2cdb161fc33132c
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2024-02-16 17:19:10 +0900

    [Silver III] Title: N과 M (4), Time: 160 ms, Memory: 15516 KB -BaekjoonHub

---
+++ 백준/Silver III/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    static int[] a = new int[10];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void go(int idx, int start, int n, int m) {
+        if (idx == m) {
+            for (int i=0; i<m; i++){
+                sb.append(a[i]);
+                if (i != m-1) sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i=start; i<=n; i++) {
+            a[idx] = i;
+            go(idx+1, i, n, m);
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]);
+        go(0,1,  n, m);
+        System.out.println(sb);
+
+    }
+}
