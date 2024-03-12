2acac95dcc995caf36212816f9ce802b505157f2
Yeonuel <87258372+jongheonleee@users.noreply.github.com>
2024-03-12 10:06:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,56 +1,82 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
 
+
+/**
+문제 요약
+
+ 괄호를 적절히 쳐서 이 식의 값을 최소로 만들려고 한다.
+
+ */
 
 public class Main {
 
+    /**
+     접근법
 
-    public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int ans = 0;
+     > 그리디, - 가 관건
+
+     1. -가 나오면 다음 -가 나오기 전까지 해당 범위 () 처리
+        ()처리용 체킹이 필요
+
+     2. 전체합, 부분합, 숫자 기록용 변수 선언
+
+     */
+
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
 
         String line = br.readLine();
-        boolean parentheses = false;
-        int sum = 0, num = 0;
+        int totalSum = 0, partSum = 0, number = 0;
+        boolean check = false;
 
         for (char ch : line.toCharArray()) {
+            // +
             if (ch == '+') {
-                if (parentheses == true) {
-                    sum += num;
-                }
-                else {
-                    ans += num;
-                }
-                num = 0;
-            } else if (ch == '-') {
-                if (parentheses == false) {
-                    ans += num;
-                    parentheses = true;
+                if (check) {
+                    partSum += number;
                 } else {
-                    sum += num;
-                    ans -= sum;
+                    totalSum += number;
                 }
-                num = 0;
-                sum = 0;
-            } else {
-                num = num*10 + (ch - '0');
+                number = 0;
+            }
+            // -
+            else if (ch == '-') {
+                if (check) {
+                    partSum += number;
+                    totalSum -= partSum;
+                } else {
+                    check = true;
+                    totalSum += number;
+                }
+                partSum = 0;
+                number = 0;
+            }
+            // 숫자
+            else {
+                number = number * 10 + (ch-'0');
             }
         }
 
-        if (num != 0) {
-            if (parentheses == true) {
-                sum += num;
+        if (number > 0) {
+            if (check) {
+                partSum += number;
             } else {
-                ans += num;
+                totalSum += number;
             }
         }
-
-        if (sum != 0) {
-            ans -= sum;
+        
+        if (partSum > 0) {
+            totalSum -= partSum;
         }
-        System.out.println(ans);
-
-
-
+        
+        System.out.println(totalSum);
     }
 }
